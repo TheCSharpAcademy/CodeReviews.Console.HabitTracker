8b6b65e78789309a2b4536a87aede36eb6f24bb1
kraven88 <k.rowecki@gmail.com>
2023-01-27 11:40:26 +0100

    Created the project, created models to track habits

---
+++ ConsoleHabitTracker.kraven88/Models/DailyProgress.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHabitTracker.kraven88.Models;
+internal class DailyProgress
+{
+    public int Id { get; set; }
+    public DateOnly Date { get; set; }
+    public int Quantity { get; set; }
+}
