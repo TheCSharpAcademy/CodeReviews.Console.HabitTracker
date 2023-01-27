02b219b54425367c9c3321058deacfcba5a12577
kraven88 <k.rowecki@gmail.com>
2023-01-27 12:18:36 +0100

    Added SqliteDB class
    SqliteDB class will handle all database operations

---
+++ ConsoleHabitTracker.kraven88/DataAccess/SqliteDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHabitTracker.kraven88.DataAccess;
+internal class SqliteDB
+{
+    public readonly string connectionString;
+
+	public SqliteDB(string connectionString)
+	{
+		this.connectionString = connectionString;
+	}
+
+	public void CreateTable(string tableName)
+	{
+		using (var connection = new SQLiteConnection(connectionString))
+		{
+			connection.Open();
+
+			var sql = connection.CreateCommand();
+			sql.CommandText = 
+				@$"CREATE TABLE IF NOT EXISTS {tableName}(
+					Id INTEGER [PRIMARY KEY] [NOT NULL] [AUTOINCREMENT],
+					Date TEXT,
+					Quantity INTEGER)";
+
+			sql.ExecuteNonQuery();
+
+			connection.Close();
+		}
+	}
+}
