ef03d4d5286133c8ed0a16df02ed50f284be3025
LucianoNicolasArrieta <lucianoarrieta077@gmail.com>
2023-03-18 02:41:11 -0300

    Starting with habit tracker project: Table creation

---
+++ LucianoNicolasArrieta.HabitTracker/LucianoNicolasArrieta.HabitTracker/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SQLite;
+
+namespace LucianoNicolasArrieta.HabitTracker
+{
+    class Database
+    {
+        public SQLiteConnection myConnection;
+
+        public Database()
+        {
+            myConnection = new SQLiteConnection("Data Source=habit-tracker.db");
+
+            if (!File.Exists("./habit-tracker.db"))
+            {
+                SQLiteConnection.CreateFile("habit-tracker.db");
+                Console.WriteLine("Database file created.");
+            }
+
+
+        }
+    }
+}
---
+++ LucianoNicolasArrieta.HabitTracker/LucianoNicolasArrieta.HabitTracker/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using LucianoNicolasArrieta.HabitTracker;
+using System.Data.SQLite;
+
+namespace habit_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database database = new Database();
+
+            database.myConnection.Open();
+
+            string createTableQuery = 
+                @"CREATE TABLE IF NOT EXISTS habits (
+                    Habit_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    Description TEXT NOT NULL, 
+                    Date TEXT,
+                    Quantity INTEGER
+                    )";
+
+            SQLiteCommand command = new SQLiteCommand(createTableQuery, database.myConnection);
+            command.ExecuteNonQuery();
+
+            database.myConnection.Close();
+
+        }
+    }
+}
