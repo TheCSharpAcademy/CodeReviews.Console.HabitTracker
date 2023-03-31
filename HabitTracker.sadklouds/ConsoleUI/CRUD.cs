2b0249653a89a714eb245db13803c34aab76d79e
sadklouds <sadklouds@gmail.com>
2023-03-31 18:56:14 +0100

    first version

---
+++ HabitTracker.sadklouds/ConsoleUI/CRUD.cs
@@ -0,0 +1,73 @@
+﻿using ConsoleUI.Helpers;
+using HabbitTrackerLibrary.sadklouds;
+
+namespace ConsoleUI
+{
+    internal class CRUD
+    {
+        private readonly SqliteDataAccess _db;
+
+        public CRUD(SqliteDataAccess db)
+        {
+            _db = db;
+        }
+
+        public void GetHabitRecords()
+        {
+            try
+            {
+                var habit = _db.LoadHabit();
+                if (habit.Count != 0)
+                {
+                    Console.WriteLine($"Tracking Amount of water drunken");
+                    Console.WriteLine("---------------------------------------");
+                    foreach (var record in habit)
+                    {
+                        Console.WriteLine($"Index:{record.Id}, Date:{record.Date.ToShortDateString()}, Quantity:{record.Quantity}  Unit:{record.Unit}");
+                        Console.WriteLine("---------------------------------------------------------------------");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No record were found");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\nHabit could not be found!\n");
+            }
+        }
+        public void UpdateHabitRecord()
+        {
+            int id = (int)UserInputsHelper.GetDoubleInput("Please enter record ID to update: ");
+            string date = UserInputsHelper.GetHabitDateInput();
+            double quantity = UserInputsHelper.GetDoubleInput("Enter quantity of habit: ");
+            string unit = UserInputsHelper.GetStringInput("Enter unit of measure (KM, miles, number of glasses etc) ");
+            string result = _db.UpdateRecord(id, date, quantity, unit);
+            Console.WriteLine(result);
+        }
+
+        public void DeleteHabitRecord()
+        {
+            int id = (int)UserInputsHelper.GetDoubleInput("Please enter record ID for deletion: ");
+            string result = _db.DeleteRecord(id);
+            Console.WriteLine(result);
+        }
+        public void AddHabitRecord()
+        {
+            string date = UserInputsHelper.GetHabitDateInput();
+            double quantity = UserInputsHelper.GetDoubleInput("Enter quantity of habit: ");
+            string unit = UserInputsHelper.GetStringInput("Enter unit of measure (KM, miles, number of glasses etc) ");
+            try
+            {
+                _db.InsertHabit(date, quantity, unit);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"\nHabit name was not found\n");
+            }
+
+        }
+
+    }
+}
---
+++ HabitTracker.sadklouds/ConsoleUI/Program.cs
@@ -0,0 +1,67 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using HabbitTrackerLibrary;
+using HabbitTrackerLibrary.sadklouds;
+using ConsoleUI;
+using ConsoleUI.Helpers;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        SqliteDataAccess db = new(GetConnectionString());
+        db.ConnectDB();
+        CRUD crud = new(db);
+
+
+        int Operation;
+        while (true)
+        {
+            MenuHelper.MainMenu();
+            Console.Write("Which operation do you wish to perform?: ");
+            string input = Console.ReadLine();
+
+            if (Int32.TryParse(input, out Operation))
+            {
+                switch (Operation)
+                {
+                   
+                    case 1:
+                        crud.GetHabitRecords();
+                        break;
+                    case 2:
+                        crud.AddHabitRecord();
+                        break;
+                    case 3:
+                        crud.DeleteHabitRecord();
+                        break;
+                    case 4:
+                        crud.UpdateHabitRecord();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Exiting Habit Tracker...");
+                        return;
+                    default:
+                        Console.WriteLine("Unknown command was given.");
+                        break;
+                }
+            }
+
+        }
+
+    }
+    private static string GetConnectionString(string connectionStringName = "Default")
+    {
+        string output = "";
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+        var config = builder.Build();
+
+        output = config.GetConnectionString(connectionStringName);
+        return output;
+    }
+}
