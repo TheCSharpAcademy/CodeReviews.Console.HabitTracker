e5d2994aae5620f57e5a14c9fad3dbebeff5f4d0
barakisbrown <lokislayer@gmail.com>
2023-04-20 22:42:50 -0500

    POCO/MODEL CLASS

---
+++ HabitTracker.barakisbrown/HabitTracker.barakisbrown/Habit.cs
@@ -0,0 +1,14 @@
+﻿namespace HabitTracker.barakisbrown;
+
+internal class Habit
+{
+    public readonly string HabitName = "BloodSugar-Readings";
+    public int id { get; set; }
+    public DateTime Date { get; set; }
+    public int Amount { get; set; }
+
+    public override string ToString()
+    {
+        return $"Id = {id}, Date = {Date}, Amount = {Amount}";
+    }
+}
