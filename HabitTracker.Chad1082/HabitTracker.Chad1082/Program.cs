d13d9ceb0cd8c5ede95042c7337227183ddcc88b
Chad1082 <ian.chadburn@hotmail.co.uk>
2023-04-21 17:44:03 +0100

    Logger Completed

---
+++ HabitTracker.Chad1082/HabitTracker.Chad1082/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HabitTracker.Chad1082
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetupDB(); 
+
+            Menu mainMenu = new Menu();
+
+            mainMenu.ShowMainMenu();
+        }
+    }
+}
