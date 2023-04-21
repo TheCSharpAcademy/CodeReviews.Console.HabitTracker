d10aa0485060f920f38e9da0c7483fd22726ec14
barakisbrown <lokislayer@gmail.com>
2023-04-20 22:42:30 -0500

    DB Stuff Here

---
+++ HabitTracker.barakisbrown/HabitTracker.barakisbrown/DataLayer.cs
@@ -0,0 +1,7 @@
+﻿namespace HabitTracker.barakisbrown;
+
+internal class DataLayer
+{
+    private readonly string DatabaseName = "readings.db";
+    private readonly string TableName = "Sugar Readings";
+}
