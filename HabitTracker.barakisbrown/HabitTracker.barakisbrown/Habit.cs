0009c039191ad8f899fdeed67e953ffc0fa80dfb
barakisbrown <lokislayer@gmail.com>
2023-04-23 17:56:21 -0500

    Model Class Updated

--- HabitTracker.barakisbrown/HabitTracker.barakisbrown/Habit.cs
+++ HabitTracker.barakisbrown/HabitTracker.barakisbrown/Habit.cs
@@ -1,14 +1,14 @@
 ﻿namespace HabitTracker.barakisbrown;
 
-internal class Habit
+public class Habit
 {
     public readonly string HabitName = "BloodSugar-Readings";
     public int id { get; set; }
-    public DateTime Date { get; set; }
     public int Amount { get; set; }
+    public DateTime Date { get; set; }
 
     public override string ToString()
     {
-        return $"Id = {id}, Date = {Date}, Amount = {Amount}";
+        return $"Id = {id}, Date = {Date.ToShortDateString()}, Amount = {Amount}";
     }
 }
