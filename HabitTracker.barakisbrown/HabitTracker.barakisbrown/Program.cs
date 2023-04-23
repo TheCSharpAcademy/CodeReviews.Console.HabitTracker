42737f42865d5f3cf7cd6ca9e98e9e05813b0588
barakisbrown <lokislayer@gmail.com>
2023-04-23 17:57:07 -0500

    Ok with current state

--- HabitTracker.barakisbrown/HabitTracker.barakisbrown/Program.cs
+++ HabitTracker.barakisbrown/HabitTracker.barakisbrown/Program.cs
@@ -1,6 +1,24 @@
 ﻿using HabitTracker.barakisbrown;
+using System.Collections.Generic;
+using System.Globalization;
 
+/*
+ * Check to see if DB Exist. 
+ * Exist => True then Display the Menu
+ * Exist => False -> Create the DB and Initial Table
+ */
 Helpers help = new();
+DataLayer data = new();
+/*
+Habit habit = new();
+habit.Amount = help.GetAmount();
+habit.Date = help.GetDate();
+data.Insert(habit);
+*/
 
-help.GetMenu();
-var date = help.GetDate();
+
+List<Habit> habits = data.SelectAll();
+foreach (var hab in habits)
+{
+    Console.WriteLine(hab);
+}
