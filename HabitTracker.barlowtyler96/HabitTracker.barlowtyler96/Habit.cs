fc4ed52e7d165ef5e3937556fd2d8e983ac834f3
Tyler Barlow <barlowtyler96@gmail.com>
2023-05-02 16:45:03 -0400

    Finished Project

---
+++ HabitTracker.barlowtyler96/HabitTracker.barlowtyler96/Habit.cs
@@ -0,0 +1,15 @@
+﻿namespace HabitTracker
+{
+    internal class Habit
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Activity { get; set; }
+
+        public string Unit { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
