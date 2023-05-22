9181c03711e5e1c404e6d510a030d36335d4bdca
furiax <carlmalfliet@proximus.be>
2023-05-22 13:30:05 +0200

    created connection

---
+++ HabitTrackerFuriax/HabitTrackerFuriax/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habitTracker.db";
+using (var connection = new SqliteConnection(connectionString))
+{
+	connection.Open();
+	var command = connection.CreateCommand();
+
+	command.CommandText = "";
+	command.ExecuteNonQuery();
+	connection.Close();
+}
