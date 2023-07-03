0b1f84c532580b28c6dd5ab58fcd039fd71a64e9
Álvaro Mosconi <alvaromosconi@outlook.com>
2023-07-03 01:15:08 -0300

    identantion fixed according to website conventions

--- HabitTracker.alvaromosconi/HabitTracker.alvaromosconi/Habit.cs
+++ HabitTracker.alvaromosconi/HabitTracker.alvaromosconi/Habit.cs
@@ -1,14 +1,13 @@
 ﻿using System.Diagnostics.Metrics;
 
-namespace HabitTracker.alvaromosconi
-{
-    internal class Habit
-    { 
-        public int Id { get; set; }
-        public string Name { get; set; }
-            
-        public DateTime Date { get; set; }
+namespace HabitTracker.alvaromosconi;
 
-        public int Quantity { get; set; }
-    }
+internal class Habit
+{ 
+    public int Id { get; set; }
+    public string Name { get; set; }
+        
+    public DateTime Date { get; set; }
+
+    public int Quantity { get; set; }
 }
