35011f9b55b6723172730b7f5e3bc69244292ac1
Moisés Macedo <mo3ses404@gmail.com>
2023-07-04 00:16:00 -0300

    fix:removed the 6 using issues from codacy

--- HabitTracker.Mo3ses/DbConn.cs
+++ HabitTracker.Mo3ses/DbConn.cs
@@ -1,11 +1,7 @@
 ﻿using ConsoleTableExt;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SQLite;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HabitTracker.Mo3ses
 {
--- HabitTracker.Mo3ses/Menu.cs
+++ HabitTracker.Mo3ses/Menu.cs
@@ -1,9 +1,5 @@
 ﻿using ConsoleTableExt;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace HabitTracker.Mo3ses
 {
