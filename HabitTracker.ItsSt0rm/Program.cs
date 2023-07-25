fd15cfcc8462634319c4980a74e4a8a20785e5a8
Storm <esteban.palaciov@hotmail.com>
2023-07-24 22:30:28 -0500

    Validations

--- HabitTracker.ItsSt0rm/Program.cs
+++ HabitTracker.ItsSt0rm/Program.cs
@@ -53,6 +53,7 @@ namespace HabitTracker.ItsSt0rm
                     case "0":
                         Console.WriteLine("\nGoodbye!\n");
                         closeApp = true;
+                        Environment.Exit(0);
                         break;
                     case "1":
                         GetAllRecords();
@@ -202,11 +203,17 @@ namespace HabitTracker.ItsSt0rm
 
         internal static string GetDateInput()
         {
-            Console.WriteLine("\n\nPlease insert the date: (Format: dd-mm-yy. Type 0 to return to main menu");
+            Console.WriteLine("\n\nPlease insert the date: (Format: dd-mm-yy). Type 0 to return to main menu");
 
             string dateInput = Console.ReadLine();
 
             if (dateInput == "0") GetUserInput();
+
+            while (!DateTime.TryParseExact(dateInput, "dd-MM-yy", new CultureInfo("en-US"), DateTimeStyles.None, out _))
+            {
+                Console.WriteLine("\n\nInvalid date. (Format: dd-mm-yy). Type 0 to return to main menu or try again:\n\n");
+                dateInput = Console.ReadLine();
+            }
 
             return dateInput;
         }
@@ -218,6 +225,12 @@ namespace HabitTracker.ItsSt0rm
             string numberInput = Console.ReadLine();
 
             if (numberInput == "0") GetUserInput();
+
+            while (!Int32.TryParse(numberInput, out _) || Convert.ToInt32(numberInput) < 0)
+            {
+                Console.WriteLine("\n\nInvalid number. Try again.\n\n");
+                numberInput = Console.ReadLine();
+            }
 
             int finalInput = Convert.ToInt32(numberInput);
 
