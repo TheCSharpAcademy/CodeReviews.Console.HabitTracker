b4ed518b514f8bdbbe219e472ee3185bafcd0566
Roman <r.petliak12@gmail.com>
2023-08-08 19:37:16 -0400

    Test

---
+++ HabitTracker.KdRome/HabitTracker.KdRome/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Data.Sqlite;
+using System.Data.SqlTypes;
+
+Console.WriteLine("Hello, World!");
+
+string connectionString = @"Data Source=HabitTracker.db";
+
+
+using (var connection = new SqliteConnection(connectionString)) {
+
+
+
+}
