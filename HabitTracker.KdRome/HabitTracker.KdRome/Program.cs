9c122b53207193030a857eae206207bb6708c321
Roman <r.petliak12@gmail.com>
2023-08-08 20:03:56 -0400

    test

--- HabitTracker.KdRome/HabitTracker.KdRome/Program.cs
+++ HabitTracker.KdRome/HabitTracker.KdRome/Program.cs
@@ -8,7 +8,14 @@ string connectionString = @"Data Source=HabitTracker.db";
 
 
 using (var connection = new SqliteConnection(connectionString)) {
+     //opens the connection
+     connection.Open();
+     //table command
+     var tableCmd = connection.CreateCommand();
 
+     tableCmd.CommandText = "";
 
+     tableCmd.ExecuteNonQuery(); //means that the data base will not return any values
 
+     connection.Close();
 }
