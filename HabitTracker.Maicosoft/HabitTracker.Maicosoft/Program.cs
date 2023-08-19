61dacbed3f5bd18ad0da9257184dcf8668d517a1
Maico Silva <maico1978@outlook.com>
2023-08-20 00:03:14 +0200

    Ceated first db and table

--- HabitTracker.Maicosoft/HabitTracker.Maicosoft/Program.cs
+++ HabitTracker.Maicosoft/HabitTracker.Maicosoft/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habit-Tracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+
+    var tableCmd = connection.CreateCommand();
+
+    tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS drinking_water (
+                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                Date TEXT,
+                                Quantity INTEGER
+                                )";
+
+    tableCmd.ExecuteNonQuery();
+
+    connection.Close();
+}
