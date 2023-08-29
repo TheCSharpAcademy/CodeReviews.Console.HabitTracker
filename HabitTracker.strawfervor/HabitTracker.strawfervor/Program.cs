a851762da9f6fcd4c5df097c54fd1ea27d8e6850
strawfervor <strawfervor@gmail.com>
2023-08-29 17:41:39 +0100

    nuget fixed

---
+++ HabitTracker.strawfervor/HabitTracker.strawfervor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace habit_tracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! This is habbit tracker! Wow!");
+        }
+    }
+}
