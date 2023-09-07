73206f617ef35861c20198dd83b1a99d5fa6b65c
Kakurokan <67524990+kakurokan@users.noreply.github.com>
2023-09-08 00:13:53 +0100

    First upload

---
+++ HabitLogger,Kakurokan/DataAcces.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.SQLite;
+
+namespace Habit_Logger.Kakurokan
+{
+    internal class DataAcces
+    {
+        public SQLiteConnection MyConnection { get; private set; }
+
+        public DataAcces()
+        {
+            MyConnection = new("Data Source=habit_logger.db;Version=3;");
+        }
+
+    }
+}
---
+++ HabitLogger,Kakurokan/Habit.cs
@@ -0,0 +1,32 @@
+﻿namespace HabitLogger_Kakurokan
+{
+    internal class Habit
+    {
+        public string Name { get; private set; }
+        public DateTime Date { get; private set; }
+        public int Quantity { get; private set; }
+        public int Id { get; private set; }
+        public string Unit { get; private set; }
+
+        public Habit(string name, DateTime date, int quantity, int id, string unit)
+        {
+            Name = name;
+            Date = date;
+            Quantity = quantity;
+            Id = id;
+            this.Unit = unit;
+        }
+
+        public Habit()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $@" Id : {Id}
+ Habit: {Name} 
+ At: {Date:dd-MM-yyyy}
+ Quantity: {Quantity}{Unit}";
+        }
+    }
+}
