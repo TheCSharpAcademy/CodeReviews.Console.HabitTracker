af45ec4230fc3eb177e784a986e4b83b547e4f9c
Andreas Abaidoo <abaidooandy@gmail.com>
2023-09-25 23:30:01 +0000

    Initial Setup

---
+++ HabbitLogger.AndreasGuy54/HabbitLogger.AndreasGuy54/Helpers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabbitLogger.AndreasGuy54
+{
+    internal static class Helpers
+    {
+        internal static void GetUserInput()
+        {
+            Console.Clear();
+
+            bool closeApp = false;
+
+            while(!closeApp)
+            {
+                Console.WriteLine("\n\nMain Menu");
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("\nType 0 to Close Application");
+                Console.WriteLine("Type 1 to View All Records");
+                Console.WriteLine("Type 2 to Insert Record");
+                Console.WriteLine("Type 3 to Delete Record");
+                Console.WriteLine("Type 4 to Update Record");
+                Console.WriteLine("------------------------------------------\n");
+
+                int userInput;
+                bool validInput = int.TryParse(Console.ReadLine().ToLower().Trim(), out userInput);
+
+                while(!validInput) 
+                {
+                    Console.WriteLine("Enter a valid number");
+                    validInput = int.TryParse(Console.ReadLine().ToLower().Trim(), out userInput);
+                }
+
+                switch (userInput)
+                {
+                    case 0:
+                        Console.WriteLine("\nGoodbye:\n");
+                        closeApp = true;
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ HabbitLogger.AndreasGuy54/HabbitLogger.AndreasGuy54/Program.cs
@@ -0,0 +1,23 @@
+﻿using HabbitLogger.AndreasGuy54;
+using Microsoft.Data.Sqlite;
+
+Console.WriteLine("Hello, World!");
+
+string connectionString = @"Data Source=habit_tracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+
+    var tableCmd = connection.CreateCommand();
+    tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS drinking_water(
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT,
+            Quantity INTEGER)";
+
+    tableCmd.ExecuteNonQuery();
+
+    connection.Close();
+}
+
+Helpers.GetUserInput();
