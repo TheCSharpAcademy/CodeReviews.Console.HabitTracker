9ca8662fd9a384aee30506dd67f8d3537969a747
Andreas Abaidoo <abaidooandy@gmail.com>
2023-09-26 10:25:21 +0000

    Insert and Read Functionality

--- HabbitLogger.AndreasGuy54/HabbitLogger.AndreasGuy54/Helpers.cs
+++ HabbitLogger.AndreasGuy54/HabbitLogger.AndreasGuy54/Helpers.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +10,14 @@ namespace HabbitLogger.AndreasGuy54
 {
     internal static class Helpers
     {
+        static string connectionString = @"Data Source=habit_tracker.db";
         internal static void GetUserInput()
         {
             Console.Clear();
 
             bool closeApp = false;
 
-            while(!closeApp)
+            while (!closeApp)
             {
                 Console.WriteLine("\n\nMain Menu");
                 Console.WriteLine("\nWhat would you like to do?");
@@ -28,7 +31,7 @@ namespace HabbitLogger.AndreasGuy54
                 int userInput;
                 bool validInput = int.TryParse(Console.ReadLine().ToLower().Trim(), out userInput);
 
-                while(!validInput || userInput < 0)
+                while (!validInput || userInput < 0)
                 {
                     Console.WriteLine("Enter a valid number");
                     validInput = int.TryParse(Console.ReadLine().ToLower().Trim(), out userInput);
@@ -41,11 +44,118 @@ namespace HabbitLogger.AndreasGuy54
                         closeApp = true;
                         Environment.Exit(0);
                         break;
-
+                    case 1:
+                        ShowAllRecords();
+                        break;
+                    case 2:
+                        InsertRecord();
+                        break;
                     default:
                         break;
                 }
             }
+        }
+
+        internal static void ShowAllRecords()
+        {
+            Console.Clear();
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+                tableCmd.CommandText = $"SELECT * FROM drinking_water";
+
+                List<Water> tableData = new();
+
+                SqliteDataReader reader = tableCmd.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        if (DateTime.TryParseExact(reader.GetString(1), "dd-MM-yy", new CultureInfo("en-UK"), DateTimeStyles.None, out DateTime date))
+                        {
+                            tableData.Add(
+                                new Water
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Date = date,
+                                    Quantity = reader.GetInt32(2)
+                                }
+                            );
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Invalid date format found: {reader.GetString(1)}");
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No records found");
+                }
+
+                connection.Close();
+
+                Console.WriteLine("----------------------------------\n");
+                foreach (var dw in tableData)
+                {
+                    Console.WriteLine($"{dw.Id} - {dw.Date.ToString("dd-MM-yyyy")} - Quantity: {dw.Quantity}");
+                }
+                Console.WriteLine("-----------------------------------\n");
+            }
+        }
+
+        internal static void InsertRecord()
+        {
+            string date = GetDateInput();
+
+            int quantity = GetNumberInput("\n\nPlease insert the number of glasses or other unit of measure of your choice (no decimals allowed)\n\n");
+
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var insertCmd = connection.CreateCommand();
+                insertCmd.CommandText = $@"INSERT INTO drinking_water(date, quantity)
+                    VALUES('{date}',{quantity})";
+
+                insertCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+        internal static string GetDateInput()
+        {
+            Console.WriteLine("\n\nPlease insert the date: (Format: dd-mm-yy). Type 0 to return to Main Menu.\n\n");
+            string dateInput = Console.ReadLine();
+
+            if (dateInput.Length == 0)
+                GetUserInput();
+
+            while (!DateTime.TryParseExact(dateInput, "dd-MM-yy", new CultureInfo("en-UK"), DateTimeStyles.None, out DateTime date))
+            {
+                Console.WriteLine("\n\nInvalid data. (Format: dd-mm-yy). Type 0 to return to main menu or try again:\n\n");
+                dateInput = Console.ReadLine();
+            }
+
+            return dateInput;
+        }
+
+        internal static int GetNumberInput(string message)
+        {
+            Console.WriteLine(message);
+            string numberInput = Console.ReadLine().ToLower().Trim();
+
+            while (!int.TryParse(numberInput, out _) || Convert.ToInt32(numberInput) < 0)
+            {
+                Console.WriteLine("\n\nInvalid Input. Enter a number");
+                numberInput = Console.ReadLine();
+            }
+
+            int finalInput = Convert.ToInt32(numberInput);
+            return finalInput;
         }
     }
 }
---
+++ HabbitLogger.AndreasGuy54/HabbitLogger.AndreasGuy54/Water.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabbitLogger.AndreasGuy54
+{
+    internal class Water
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Quantity { get; set; }
+    }
+}
