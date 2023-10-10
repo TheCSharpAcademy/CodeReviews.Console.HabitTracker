89b6d6a78e81633f1b6479c5dfb943b7620d3e7f
UgniusFalze <ugnius.vilkoncius@gmail.com>
2023-10-10 20:58:08 +0300

    Initial Project without readme

---
+++ HabitTracker.UgniusFalze/HabitTracker.UgniusFalze/Program.cs
@@ -0,0 +1,251 @@
+﻿using HabitTracker.UgniusFalze;
+
+
+void tracker()
+{
+    bool exitTracker = false;
+    Display.DisplayIntroMessage();
+    SQLLiteOperations sQLLiteOperations = new SQLLiteOperations();
+
+    do
+    {
+        Display.DisplaySeperator();
+        Display.DisplayMenu();
+        string input = Console.ReadLine();
+        try
+        {
+            int option = Int32.Parse(input);
+            switch (option)
+            {
+                case 1:
+                    List<Habbit> habbits = sQLLiteOperations.GetHabbitHistory();
+                    Display.DisplayAllHabbits(habbits);
+                    break;
+                case 2:
+                    handleInput(sQLLiteOperations);
+                    break;
+                case 3:
+                    handleDelete(sQLLiteOperations);
+                    break;
+                case 4:
+                    handleUpdate(sQLLiteOperations);
+                    break;
+                case 0: exitTracker = true; break;
+                default: Display.DisplayIncorrectMenuOption(); break;
+            }
+        }
+        catch(Exception e)
+        {
+            if (e is ArgumentNullException || e is FormatException || e is OverflowException)
+            {
+                Display.DisplayIncorrectNumber();
+            }
+        }
+
+    } while (!exitTracker);
+}
+
+void handleInput(SQLLiteOperations sQLLiteOperations)
+{
+    Display.DisplaySeperator();
+    try
+    {
+        DateOnly date = (DateOnly)handleDateInput();
+        long quantity = (long)handleQuantityInput();
+        sQLLiteOperations.InsertHabbit(quantity, date);
+    }
+    catch (Exception e)
+    {
+        if (e is ArgumentNullException)
+        {
+            return;
+        }
+        Display.SQLLiteException();
+        return;
+    }
+}
+
+void handleDelete(SQLLiteOperations sQLLiteOperations)
+{
+    Display.DisplayDeleteHabbit();
+    Display.DisplaySeperator();
+    Display.DisplayAllHabbits(sQLLiteOperations.GetHabbitHistory());
+    bool exitDelete = false;
+
+    do
+    {
+        try
+        {
+            string input = Console.ReadLine();
+            long id = Int64.Parse(input);
+            if (id <= 0)
+            {
+                throw new FormatException();
+            }
+            sQLLiteOperations.DeleteHabbit(id);
+            exitDelete = true;
+        }
+        catch (Exception e)
+        {
+            if (e is ArgumentNullException || e is FormatException || e is OverflowException)
+            {
+                Display.DisplayIncorrectNumber();
+            }
+            else
+            {
+                Display.IncorrectId();
+            }
+        }
+
+
+    } while (!exitDelete);
+}
+
+void handleUpdate(SQLLiteOperations sQLLiteOperations)
+{
+    Display.DisplaySeperator();
+    Display.DisplayWhichRecordToUpdate();
+    Display.DisplayAllHabbits(sQLLiteOperations.GetHabbitHistory());
+    long id = -1;
+    bool exitHabbitChoice = false;
+
+    do
+    {
+        try
+        {
+            string input = Console.ReadLine();
+            id = Int64.Parse(input);
+            if (id <= 0)
+            {
+                throw new FormatException();
+            }
+            sQLLiteOperations.CheckIfRecordWithIdExsists(id);
+            exitHabbitChoice = true;
+        }
+        catch (Exception e)
+        {
+            if (e is ArgumentNullException || e is FormatException || e is OverflowException)
+            {
+                Display.DisplayIncorrectNumber();
+            }
+            else
+            {
+                Display.IncorrectId();
+            }
+        }
+
+
+    } while (!exitHabbitChoice);
+
+    if (id > 0)
+    {
+        Display.DisplaySeperator();
+        Display.DisplayUpdateMenu();
+
+        bool updateLoop = false;
+
+        do
+        {
+            try
+            {
+                string menuInput = Console.ReadLine();
+                int menuChoice = Int32.Parse(menuInput);
+                switch (menuChoice)
+                {
+                    case 1:
+                        var quantityInput = handleQuantityInput();
+                        if (quantityInput == null)
+                        {
+                            return;
+                        }
+                        long quantity = (long)quantityInput;
+                        sQLLiteOperations.UpdateQuantity(id, quantity);
+                        return;
+                    case 2:
+                        var dateInput = handleDateInput();
+                        if (dateInput == null)
+                        {
+                            return;
+                        }
+                        DateOnly dateOnly = (DateOnly)dateInput;
+                        sQLLiteOperations.UpdateDate(id, dateOnly);
+                        return;
+                    case 0:
+                        return;
+                }
+            }catch(Exception e)
+            {
+                if (e is ArgumentNullException || e is FormatException || e is OverflowException)
+                {
+                    Display.DisplayIncorrectNumber();
+                }
+                else
+                {
+                    Display.SQLLiteException();
+                    return;
+                }
+            }
+
+        } while (!updateLoop);
+    }
+}
+
+DateOnly? handleDateInput()
+{
+    Display.DisplayDateInput();
+    do
+    {
+        try
+        {
+            string input = Console.ReadLine();
+            DateOnly dateOnly = DateOnly.ParseExact(input, SQLLiteOperations.GetDateFormat());
+            return dateOnly;
+        }
+        catch (Exception e)
+        {
+            if (e is ArgumentNullException || e is FormatException)
+            {
+                Display.DisplayIncorrectFormat();
+            }
+            else
+            {
+                Display.IOException();
+                return null;
+            }
+        }
+
+    } while (true);
+}
+long? handleQuantityInput()
+{
+
+    Display.DisplayQuantityInput();
+    do
+    {
+        try
+        {
+            string input = Console.ReadLine();
+            long quantity = Int64.Parse(input);
+            if (quantity < 0)
+            {
+                throw new FormatException();
+            }
+            return quantity;
+        }
+        catch (Exception e)
+        {
+            if (e is ArgumentNullException || e is FormatException || e is OverflowException)
+            {
+                Display.DisplayIncorrectNumber();
+            }
+            else
+            {
+                Display.IOException();
+                return null;
+            }
+        }
+
+    } while (true);
+}
+
+tracker();
---
+++ HabitTracker.UgniusFalze/HabitTracker.UgniusFalze/SQLLiteOperations.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace HabitTracker.UgniusFalze
+{
+    internal class SQLLiteOperations
+    {
+        private SqliteConnection connection;
+        public SQLLiteOperations(string dbFile = "habbitLog.db")
+        {
+            connection = new SqliteConnection($"Data Source={dbFile}");
+            connection.Open();
+            CreateHabbitTable();
+        }
+
+        public void CheckIfRecordWithIdExsists(long id)
+        {
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText = @"
+                SELECT 1 
+                FROM CupsOfCoffee 
+                WHERE Id = $id";
+            command.Parameters.AddWithValue("id", id);
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                if (reader.Read() == false)
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        private void CreateHabbitTable()
+        {
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                CREATE TABLE IF NOT EXISTS CupsOfCoffee(
+                    Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                    Quantity INTEGER NOT NULL,
+                    Date TEXT NOT NULL
+                  );
+            ";
+            command.ExecuteNonQuery();
+        }
+
+        public void InsertHabbit(long quantity, DateOnly date)
+        {
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                INSERT INTO CupsOfCoffee(quantity, Date) VALUES($quantity, $date);
+            ";
+
+            command.Parameters.AddWithValue("$quantity", quantity);
+            command.Parameters.AddWithValue("$date", date.ToString(GetDateFormat()));
+            command.ExecuteNonQuery();
+        }
+
+        public void UpdateQuantity(long id, long quantity)
+        {
+            CheckIfRecordWithIdExsists(id);
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                UPDATE CupsOfCoffee
+                SET quantity = $quantity
+                WHERE Id = $id
+                ;
+            ";
+            command.Parameters.AddWithValue("$quantity", quantity);
+            command.Parameters.AddWithValue("$id", id);
+            command.ExecuteNonQuery();
+        }
+
+        public void UpdateDate(long id, DateOnly date)
+        {
+            CheckIfRecordWithIdExsists(id);
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                UPDATE CupsOfCoffee
+                SET date = $date
+                WHERE Id = $id
+                ;
+            ";
+            command.Parameters.AddWithValue("$date", date.ToString(GetDateFormat()));
+            command.Parameters.AddWithValue("$id", id);
+            command.ExecuteNonQuery();
+        }
+        public void DeleteHabbit(long id)
+        {
+            CheckIfRecordWithIdExsists(id);
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                DELETE FROM CupsOfCoffee
+                WHERE Id = $id
+                ;
+            ";
+            command.Parameters.AddWithValue("$id", id);
+            command.ExecuteNonQuery();
+        }
+
+        public List<Habbit> GetHabbitHistory()
+        {
+            List<Habbit> habbits = new List<Habbit>();
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText =
+            @"
+                SELECT Id, Quantity, Date
+                FROM CupsOfCoffee
+                ;
+            ";
+
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    long id = reader.GetInt64(0);
+                    long quantity = reader.GetInt64(1);
+                    DateOnly date = DateOnly.ParseExact(reader.GetString(2), GetDateFormat());
+                    Habbit habbit = new(id, quantity, date);
+                    habbits.Add(habbit);
+                }
+            }
+
+            return habbits;
+        }
+
+        public static string GetDateFormat()
+        {
+            return "dd/MM/yyyy";
+        }
+    }
+}
