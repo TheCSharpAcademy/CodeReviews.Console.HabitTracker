66c3a808dff763a222d01bc364c725be0f4d8d6a
jklogimark <108735639+jklogimark@users.noreply.github.com>
2023-10-19 14:00:51 +0200

    Added HabitManager.cs class
    * Added the Habit Manager.cs class to the project to handle the responsibility for CRUD operations for habits.
    * Added some comments to the methods.

---
+++ HabitTracker.kalsson/HabitTracker.kalsson/HabitManager.cs
@@ -0,0 +1,36 @@
+﻿namespace HabitTracker.kalsson;
+
+public class HabitManager
+{
+    /// <summary>
+    /// Insert a new habit into the database.
+    /// </summary>
+    public void InsertHabit() 
+    {
+        // SQL for inserting habit
+    }
+
+    /// <summary>
+    /// Delete an existing habit from the database.
+    /// </summary>
+    public void DeleteHabit() 
+    {
+        // SQL for deleting habit
+    }
+
+    /// <summary>
+    /// Update details of an existing habit in the database.
+    /// </summary>
+    public void UpdateHabit() 
+    {
+        // SQL for updating habit
+    }
+
+    /// <summary>
+    /// View all habits from the database.
+    /// </summary>
+    public void ViewHabit() 
+    {
+        // SQL for viewing habits
+    }
+}
