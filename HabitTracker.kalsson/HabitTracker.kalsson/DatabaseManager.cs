3101315b75de36f6ce067b44d68a8bbe850f6838
jklogimark <108735639+jklogimark@users.noreply.github.com>
2023-10-19 13:57:52 +0200

    Added DatabaseManager.cs class
    * Added the DatabaseManager.cs class to the project to handle the connection and so on.
    * Added some comments to the methods.

---
+++ HabitTracker.kalsson/HabitTracker.kalsson/DatabaseManager.cs
@@ -0,0 +1,20 @@
+﻿namespace HabitTracker.kalsson;
+
+public class DatabaseManager
+{
+    /// <summary>
+    /// Initialize SQLite database if it doesn't exist.
+    /// </summary>
+    public void InitializeDatabase() 
+    {
+        // SQL to initialize database
+    }
+    
+    /// <summary>
+    /// Create table for storing habits.
+    /// </summary>
+    public void CreateTable() 
+    {
+        // SQL to create table
+    }
+}
