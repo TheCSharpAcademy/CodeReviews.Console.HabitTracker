e90ff76666b4f4560b9e06c5f36c8ee48126d09a
jklogimark <108735639+jklogimark@users.noreply.github.com>
2023-10-19 13:49:42 +0200

    Added ConsoleMessages.cs class.
    * Added the ConsoleMessages.cs class to the project to handle the UI parts.

---
+++ HabitTracker.kalsson/HabitTracker.kalsson/ConsoleMessages.cs
@@ -0,0 +1,19 @@
+﻿namespace HabitTracker.kalsson;
+
+public class ConsoleMessages
+{
+    public static void AppInformation()
+    {
+        // Display app information
+    }
+
+    public static void ShowMenu()
+    {
+        // Display menu
+    }
+    
+    public static void GetUserInput()
+    {
+        // Get and validate user input
+    }
+}
