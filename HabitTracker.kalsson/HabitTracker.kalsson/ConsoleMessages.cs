88e4f9b1f5c1737aed9c59c465d123fb20fcfef4
jklogimark <108735639+jklogimark@users.noreply.github.com>
2023-10-19 13:54:00 +0200

    ConsoleMessages.cs comments
    * Added some comments for the methods in ConsoleMessages.cs class.

--- HabitTracker.kalsson/HabitTracker.kalsson/ConsoleMessages.cs
+++ HabitTracker.kalsson/HabitTracker.kalsson/ConsoleMessages.cs
@@ -2,16 +2,25 @@
 
 public class ConsoleMessages
 {
+    /// <summary>
+    /// Displays app information to the user when the app starts.
+    /// </summary>
     public static void AppInformation()
     {
         // Display app information
     }
 
+    /// <summary>
+    /// Display available CRUD options to the user.
+    /// </summary>
     public static void ShowMenu()
     {
         // Display menu
     }
     
+    /// <summary>
+    /// Read user input for the CRUD operations.
+    /// </summary>
     public static void GetUserInput()
     {
         // Get and validate user input
