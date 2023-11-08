3d9c66bbbbb281449d5f76aa0d33d866436c6b44
czerviik <137193704+czerviik@users.noreply.github.com>
2023-11-08 14:48:03 +0100

    add avg, max and min methods as database requests, clean up

--- habitTracker.czerviik/habitTracker.czerviik/Program.cs
+++ habitTracker.czerviik/habitTracker.czerviik/Program.cs
@@ -30,6 +30,7 @@ void GetUserInput()
     bool closeApp = false;
     while (closeApp == false)
     {
+        Console.Clear();
         Console.WriteLine("\n\nMAIN MENU");
         Console.WriteLine("\nWhat would you like to do?");
         Console.WriteLine("\nType 0 to Close App.");
@@ -84,7 +85,6 @@ void GetAllRecords()
     string columnName = GetColumnName(habitName);
     List<Habit> tableData = new();
 
-
     using (var connection = new SqliteConnection(connectionString))
     {
         connection.Open();
@@ -125,14 +125,12 @@ void GetAllRecords()
     Console.WriteLine("---------------------------------------------------\n");
 }
 
-
-
 void Insert()
 {
     habitName = ChooseHabit("\nChoose a habit number from the list:\nType 0 to return to Main menu.\n");
     string date = GetDateInput();
     string columnName = GetColumnName(habitName);
-    int quantity = GetNumberInput($"\n\nPlease insert number of {columnName} for {habitName}. (no decimals allowed):\n\n");
+    int quantity = GetNumberInput($"\n\nPlease insert number of {columnName} for {habitName}. (no decimals allowed):\n\n",MenuTypes.MainMenu);
 
     using (var connection = new SqliteConnection(connectionString))
     {
@@ -161,11 +159,15 @@ string GetDateInput()
     return dateInput;
 }
 
-int GetNumberInput(string message)
+int GetNumberInput(string message, MenuTypes menu)
 {
     Console.WriteLine(message);
     string numberInput = Console.ReadLine();
-    if (numberInput == "0") GetUserInput();
+    if (numberInput == "0")
+    {
+        if (menu == MenuTypes.MainMenu) GetUserInput();
+        else if(menu == MenuTypes.QuestionMenu) QuestionMenu();
+    }
 
     while (!Int32.TryParse(numberInput, out _) || Convert.ToInt32(numberInput) < 0)
     {
@@ -186,7 +188,7 @@ void Delete()
 
     do
     {
-        recordId = GetNumberInput("\n\nPlease type the Id of the record you want to delete or type 0 to Main Menu\n");
+        recordId = GetNumberInput("\n\nPlease type the Id of the record you want to delete or type 0 to Main Menu\n",MenuTypes.MainMenu);
 
         using (var connection = new SqliteConnection(connectionString))
         {
@@ -216,12 +218,11 @@ void Update()
     GetAllRecords();
     string columnName = GetColumnName(habitName);
 
-
     using (var connection = new SqliteConnection(connectionString))
     {
         do
         {
-            recordId = GetNumberInput("\nPlease type Id of the record you would like to update. Type 0 to return to Main Menu.\n");
+            recordId = GetNumberInput("\nPlease type Id of the record you would like to update. Type 0 to return to Main Menu.\n", MenuTypes.MainMenu);
 
             connection.Open();
 
@@ -240,7 +241,7 @@ void Update()
         
 
         string date = GetDateInput();
-        int quantity = GetNumberInput($"\n\nPlease insert number of {columnName} for {habitName}. (no decimals allowed):\n\n");
+        int quantity = GetNumberInput($"\n\nPlease insert number of {columnName} for {habitName}. (no decimals allowed):\n\n", MenuTypes.MainMenu);
 
         var tableCmd = connection.CreateCommand();
         tableCmd.CommandText = $"UPDATE {habitName} SET date = '{date}', {columnName} = {quantity} WHERE Id = {recordId}";
@@ -324,7 +325,6 @@ void GetHabitsList()
 
 string ChooseHabit(string message)
 {
-    
     string result = "";
     bool inputIsValid = false;
     do
@@ -369,59 +369,161 @@ string GetColumnName(string habitName)
 }
 void QuestionMenu()
 {
+    bool questionMenuIsRunning = true;
+
     Console.Clear();
 
-    habitName = ChooseHabit("\n\nQUESTION MENU\nChoose a habit number from the list:\nType 0 to return to Main menu.\n");
+    habitName = ChooseHabit("\n\nQUESTION MENU\nChoose a habit number from the list: (Type 0 to return to Main menu.)\n");
     string columnName = GetColumnName(habitName);
-    Console.Clear();
-    Console.WriteLine($"\n\nQUESTION MENU for {habitName}");
-    Console.WriteLine("\nWhat would you like to ask?");
-    Console.WriteLine("\nType 0 to return to Main menu.");
-    Console.WriteLine($"Type 1 to get Sum of all {columnName} in a specific year.");
-    Console.WriteLine("_____________________________________________\n");
 
-    string command = Console.ReadLine();
-
-    switch (command)
+    do
     {
-        case "0":
-            GetUserInput();
-            break;
-        case "1":
-            AskQuantity(columnName); 
-            break;
-        default:
-            Console.WriteLine("\nPlease enter a number 0-3. Press any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
-            break;
-    }
+        Console.Clear();
+        Console.WriteLine($"\n\nQUESTION MENU for {habitName}");
+        Console.WriteLine("\nWhat would you like to ask?");
+        Console.WriteLine("\nType 0 to return to Main menu.");
+        Console.WriteLine($"Type 1 to get Sum of all {columnName}");
+        Console.WriteLine($"Type 2 to get Average of all {columnName}");
+        Console.WriteLine($"Type 3 to get Maximum of all {columnName}");
+        Console.WriteLine($"Type 4 to get Minimum of all {columnName}");
+        Console.WriteLine("_____________________________________________\n");
+
+        string command = Console.ReadLine();
+
+        switch (command)
+        {
+            case "0":
+                GetUserInput();
+                break;
+            case "1":
+                GetQuantityOperation(habitName, columnName, DbOperTypes.SUM);
+                break;
+            case "2":
+                GetQuantityOperation(habitName, columnName, DbOperTypes.AVG);
+                break;
+            case "3":
+                GetQuantityOperation(habitName, columnName, DbOperTypes.MAX);
+                break;
+            case "4":
+                GetQuantityOperation(habitName, columnName, DbOperTypes.MIN);
+                break;
+            default:
+                Console.WriteLine("\nPlease enter a number 0-3. Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+                break;
+        }
+    } while (questionMenuIsRunning);   
 }
 
-void AskQuantity(string columnName)
+bool SpecificYearCheck(string operatorName)
 {
-    int year = GetNumberInput("\nEnter the year you want to show:");
+    string userInput;
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine($"Do you want to see {operatorName} from a specific year? (y/n)");
 
-    using(var connection = new SqliteConnection(connectionString))
+        userInput = Console.ReadLine().ToLower();
+
+        if (userInput == "y") return true;
+        if (userInput == "n") return false;
+        
+        Console.WriteLine("Please write 'y' or 'n'");
+    }
+}
+long PerformDbQuestionOperation(string habitName, string columnName, string operationText, bool specificYear, int year)
+{
+    using (var connection = new SqliteConnection(connectionString))
     {
         connection.Open();
-        var sumCmd = connection.CreateCommand();
-        sumCmd.CommandText = $"SELECT SUM({columnName}) FROM {habitName} WHERE Date LIKE '%{year}'";
-        var result = sumCmd.ExecuteScalar();
-        if (result != DBNull.Value)
+        var operCmd = connection.CreateCommand();
+
+        if (specificYear)
         {
-            long sum = (long)result;
-            Console.WriteLine($"The sum of {columnName} in the year {year} is {sum}.");
+            operCmd.CommandText = $"SELECT {operationText} ({columnName}) FROM {habitName} WHERE Date LIKE '%{year}'";
         }
-        else Console.WriteLine("The sum cannot be calculated. Press any key to continue...");
+        else
+            operCmd.CommandText = $"SELECT {operationText} ({columnName}) FROM {habitName}";
 
-        Console.ReadKey();
-
+        var resultDb = operCmd.ExecuteScalar();
+        return resultDb != DBNull.Value ? Convert.ToInt64(resultDb) : -1;
     }
 }
+void GetQuantityOperation(string habitName, string columnName, DbOperTypes oper)
+{
+    bool inputIsValid = false;
+    string operatorName = "";
+    string operationText = "";
+    int year = 0;
+
+    switch (oper)
+    {
+        case DbOperTypes.SUM:
+            operationText = "SUM";
+            operatorName = "sum";
+            break;
+        case DbOperTypes.AVG:
+            operationText = "AVG";
+            operatorName = "average";
+            break;
+        case DbOperTypes.MAX:
+            operationText = "MAX";
+            operatorName = "maximum";
+            break;
+        case DbOperTypes.MIN:
+            operationText = "MIN";
+            operatorName = "minimum";
+            break;
+    }
+
+    bool specificYear = SpecificYearCheck(operatorName);
+
+    do
+    {
+        if (specificYear)
+            year = GetNumberInput("\nEnter the year you want to show:\nType 0 to return to Question menu.", MenuTypes.QuestionMenu);
+
+        var result = PerformDbQuestionOperation(habitName, columnName, operationText, specificYear, year);
+
+        if (result != -1)
+        {
+            if (specificYear)
+                Console.WriteLine($"The {operatorName} of {columnName} in the year {year} is {result}.");
+            else
+                Console.WriteLine($"The {operatorName} of {columnName} is {result}.");
+
+            Console.WriteLine("Press any key to return to Question menu...");
+            Console.ReadKey();
+
+            inputIsValid = true;
+        }
+        else
+        {
+            Console.WriteLine($"The {operatorName} cannot be calculated. Press any key to continue...");
+            Console.ReadKey();
+        }
+    } while (!inputIsValid);
+
+    QuestionMenu();
+}
+
 public class Habit
 {
     public int Id { get; set; }
     public DateTime Date { get; set; }
     public int Quantity { get; set; }
+}
+
+public enum MenuTypes
+{
+    MainMenu,
+    QuestionMenu
+}
+public enum DbOperTypes
+{
+    SUM,
+    AVG,
+    MAX,
+    MIN
 }
