95b84c624d612ead9d067eb61f63c832a4d2b79b
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-11-17 14:54:36 +0100

    Add update date validation

--- HabitTracker.mariusgrHiof/HabitLogger/Validate.cs
+++ HabitTracker.mariusgrHiof/HabitLogger/Validate.cs
@@ -9,6 +9,14 @@ namespace HabitLogger
             return DateTime.TryParseExact(date, "dd/MM/yyyy HH:mm", new CultureInfo("nb-NO"), DateTimeStyles.None, out _);
         }
 
+        public static bool IsValidUpdateDate(DateTime currentUpdateDate, DateTime newUpdateDate)
+        {
+            TimeSpan timeSpan = currentUpdateDate - newUpdateDate;
+
+            return timeSpan.Ticks < 0;
+        }
+
+
         public static bool IsValidNumber(string number)
         {
             return int.TryParse(number, out _);
