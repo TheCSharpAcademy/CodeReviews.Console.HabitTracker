ff0ebe7aba4aee4d4548d1d14f253439e59391ca
Ibrahim Gilani <ibigilani@gmail.com>
2023-12-30 22:52:17 -0500

    finished habit tracker app , need to add readme

--- Habit tracker.Ibrahim/Habit tracker.Ibrahim/Program.cs
+++ Habit tracker.Ibrahim/Habit tracker.Ibrahim/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using Habit_tracker.Ibrahim;
 using Microsoft.Data.Sqlite;
-using Repository;
+
 
 namespace habit_tracker;
 
@@ -10,14 +11,15 @@ class Program
     {
         string connectionString = "Data Source= HabitTracker.db";
 
-        using(var connection = new SqliteConnection(connectionString)){
+        using (var connection = new SqliteConnection(connectionString))
+        {
             connection.Open();
             var tableCmd = connection.CreateCommand();
 
-            tableCmd.CommandText = 
+            tableCmd.CommandText =
                 @"CREATE TABLE IF NOT EXISTS daily_prayer (
                 Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                Date TEXT,
+                Date TEXT UNIQUE,
                 Quantity INTEGER
                 )";
 
@@ -32,13 +34,16 @@ class Program
 
     public static void UserInput(string connectionString, DatabaseAccessLayer database)
     {
-        
+
         bool endApp = false;
+
+        Console.WriteLine("Welcome to your prayer tracker! \n");
+        Console.WriteLine("press any key to go to the main menu");
+        Console.ReadLine();
 
         while (!endApp)
         {
             Console.Clear();
-            Console.WriteLine("Welcome to your prayer tracker!");
             Console.WriteLine("Main Menu \n");
             Console.WriteLine("What would you like to do?\n");
             Console.WriteLine("Type 0 to close Application");
@@ -46,8 +51,9 @@ class Program
             Console.WriteLine("Type 2 to Insert Record");
             Console.WriteLine("Type 3 to Delete Record");
             Console.WriteLine("Type 4 to Update Record");
-            string userChoice= Console.ReadLine();
-            
+            Console.WriteLine("Type 5 to view Reports\n");
+            string userChoice = helper.ValidateChoice(Console.ReadLine());
+
 
             switch (userChoice)
             {
@@ -58,46 +64,65 @@ class Program
                 case "1":
                     Console.Clear();
                     Console.WriteLine("your prayer history \n");
-                    Console.WriteLine("Id\t Date\t\t\t Prayers \n");
+                    Console.WriteLine("Id\t Date\t\tPrayers \n");
                     database.Retrieve();
                     break;
 
                 case "2":
                     Console.Clear();
-                    Console.WriteLine(@"Type in your prayer count for today:
-minimum = 0
-maximum = 5");
-                    int prayers = Convert.ToInt32(Console.ReadLine());
 
-                    database.Insert(DateTime.UtcNow.Date.ToString(), prayers);
+                    Console.WriteLine(@"Type in your prayer count:
+minimum = 0
+maximum = 5" + "\n");
+
+                    string input = helper.ValidateInt(Console.ReadLine());
+
+                    int prayers = Convert.ToInt32(input);
+
+                    Console.WriteLine("\nType in the date you wish to enter with the format: yyyy-mm-dd");
+
+                    string date = helper.ValidateDate(Console.ReadLine());
+
+                    database.Insert(date, prayers);
                     break;
 
                 case "3":
                     Console.Clear();
-                    Console.WriteLine("Please enter the records' ID number which you want to Delete");
-                    int id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(@"Please enter the date you want to Delete in the format yyyy-mm-dd");
+                    date = helper.ValidateDate(Console.ReadLine());
 
-                    database.Delete(id);
+                    database.Delete(date);
 
-                    Console.WriteLine($"record {id} has been deleted");
+                    Console.WriteLine($"\n {date} has been deleted");
                     break;
 
                 case "4":
                     Console.Clear();
-                    Console.WriteLine("please enter the ID of the record which you want to change");
-                    id = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("please enter the new number of prayers you prayed for the day");
+                    Console.WriteLine(@"please enter the date of the record which you want to change
+Use the format: yyyy-mm-dd");
+                    date = helper.ValidateDate(Console.ReadLine());
+                    Console.WriteLine("\nplease enter the new number of prayers you prayed for the day");
                     int quantity = Convert.ToInt32(Console.ReadLine());
 
-                    database.Update(id, quantity);
+                    database.Update(date, quantity);
+                    break;
+
+                case "5":
+                    Console.Clear();
+                    Console.WriteLine("REPORTS: \n");
+                    database.getReport();
+                    // 
                     break;
                 default:
                     Console.WriteLine("entered invalid input please try again");
                     break;
 
             }
-            Console.WriteLine("press any key to go back to menu");
-            Console.ReadLine();
+            if (!endApp) 
+            { 
+                Console.WriteLine("\n press any key to go back to menu");
+                Console.ReadLine();
+            }
         }
 
 
--- Habit tracker.Ibrahim/Habit tracker.Ibrahim/Repository.cs
+++ Habit tracker.Ibrahim/Habit tracker.Ibrahim/Repository.cs
@@ -1,7 +1,6 @@
 using Microsoft.Data.Sqlite;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace Repository;
+namespace habit_tracker;
 
 public class DatabaseAccessLayer
 {
@@ -9,54 +8,107 @@ public class DatabaseAccessLayer
     SqliteConnection connection;
     public DatabaseAccessLayer(string connectionString)
     {
-
         connection = new SqliteConnection(connectionString);
+    }
+    public bool DateExists(string date)
+    {
+        connection.Open();
+        using (var cmd = connection.CreateCommand())
+        {
+            cmd.CommandText = "SELECT COUNT(*) FROM daily_prayer WHERE Date = @Date";
+            cmd.Parameters.AddWithValue("@Date", date);
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+            connection.Close();
+            return count > 0;
+        }
+    }
 
-    }
-    public  void Insert(string date, int quantity)
+    public void Insert(string date, int quantity)
     {
-        using(connection)
+        bool dateExists;
+        do
         {
-            connection.Open();
-            using (var tableCmd = connection.CreateCommand())
-            {
-                tableCmd.CommandText = "INSERT INTO daily_prayer (Date, Quantity) VALUES (@Date, @Quantity)";
-                tableCmd.Parameters.AddWithValue("@Date", date);
-                tableCmd.Parameters.AddWithValue("@Quantity", quantity);
-                tableCmd.ExecuteNonQuery();
-            }
-            connection.Close();
-        }
-    }
-    public  void Update(int Id, int quantity)
-    {
-        using (connection)
-        {
-            connection.Open();
-            using (var tableCmd = connection.CreateCommand())
-            {
-                tableCmd.CommandText = "Update daily_prayer SET Quantity = @Quantity WHERE Id = @Id";
-                tableCmd.Parameters.AddWithValue("@Id", Id);
-                tableCmd.Parameters.AddWithValue("@Quantity", quantity);
-                tableCmd.ExecuteNonQuery();
-            }
-            connection.Close();
-        }
+            dateExists = DateExists(date);
 
-    }
-    public  void Delete(int id)
-    {
-        using (connection)
-        {
-            connection.Open();
-            using (var tableCmd = connection.CreateCommand())
+            if (dateExists)
             {
-                tableCmd.CommandText = "DELETE FROM daily_prayer WHERE ID = @Id";
-                tableCmd.Parameters.AddWithValue("@Id", id);
-                tableCmd.ExecuteNonQuery();
+                Console.WriteLine($"A record with the date {date} already exists. Please enter a unique date.");
+                date = Console.ReadLine();
             }
-            connection.Close();
+
+            else
+            {
+                connection.Open();
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    tableCmd.CommandText = "INSERT INTO daily_prayer (Date, Quantity) VALUES (@Date, @Quantity)";
+                    tableCmd.Parameters.AddWithValue("@Date", date);
+                    tableCmd.Parameters.AddWithValue("@Quantity", quantity);
+                    tableCmd.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
         }
+        while (dateExists);
+    }
+
+    public void Update(string date, int quantity)
+    {
+
+        bool dateExists;
+        do
+        {
+            dateExists = DateExists(date);
+
+            if (!dateExists)
+            {
+                Console.WriteLine($"A record with the date {date} does not exist. Please enter a unique date.");
+                date = Console.ReadLine();
+            }
+
+            else
+            {
+                connection.Open();
+                using (var tableCmd = connection.CreateCommand())
+                {
+                    tableCmd.CommandText = "Update daily_prayer SET Quantity = @Quantity WHERE Date = @date";
+                    tableCmd.Parameters.AddWithValue("@date", date);
+                    tableCmd.Parameters.AddWithValue("@Quantity", quantity);
+                    tableCmd.ExecuteNonQuery();
+                }
+                connection.Close();
+            }
+        }
+        while (!dateExists);
+    }
+    public void Delete(string date)
+    {
+        bool dateExists;
+        do 
+        {
+            dateExists = DateExists(date);
+            if (dateExists)
+            {
+                using (connection)
+                {
+                    connection.Open();
+                    using (var tableCmd = connection.CreateCommand())
+                    {
+                        tableCmd.CommandText = "DELETE FROM daily_prayer WHERE Date = @date";
+                        tableCmd.Parameters.AddWithValue("@date", date);
+                        tableCmd.ExecuteNonQuery();
+                    }
+                    connection.Close();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"A record with the date {date} does not exist. Please enter a valid date.");
+                date = Console.ReadLine();
+            }
+        }
+        while (!dateExists);
+        
 
     }
     public void Retrieve()
@@ -66,11 +118,11 @@ public class DatabaseAccessLayer
             connection.Open();
             using (var tableCmd = connection.CreateCommand())
             {
-                tableCmd.CommandText = "SELECT Id, Date, Quantity FROM daily_prayer";
-                
-                using(var reader = tableCmd.ExecuteReader())
+                tableCmd.CommandText = "SELECT Id, Date, Quantity FROM daily_prayer ORDER BY Date ASC";
+
+                using (var reader = tableCmd.ExecuteReader())
                 {
-                    while (reader.Read()) 
+                    while (reader.Read())
                     {
                         int id = reader.GetInt32(0);
                         string date = reader.GetString(1);
@@ -82,6 +134,49 @@ public class DatabaseAccessLayer
             }
             connection.Close();
         }
+
+    }
+
+    public void getReport()
+    {
+        // show avg prayer for last week, year, all time
+
+
+
+        using (connection)
+        {
+            connection.Open();
+
+            // Average for the past week
+            using (var tableCmd = connection.CreateCommand())
+            {
+                tableCmd.CommandText = "SELECT AVG(Quantity) FROM daily_prayer WHERE Date >= date('now', '-7 days')";
+
+                var avgWeek = Convert.ToDouble(tableCmd.ExecuteScalar()).ToString("0");
+                Console.WriteLine("Average for past week: " + avgWeek + "/5");
+            }
+
+            // Average for the past year
+            using (var tableCmd = connection.CreateCommand())
+            {
+                tableCmd.CommandText = "SELECT AVG(Quantity) FROM daily_prayer WHERE Date >= date('now', '-1 year')";
+
+                var avgYear = Convert.ToDouble(tableCmd.ExecuteScalar()).ToString("0");
+                Console.WriteLine("Average for past year: " + avgYear + "/5");
+            }
+
+            // Average for all time
+            using (var tableCmd = connection.CreateCommand())
+            {
+                tableCmd.CommandText = "SELECT AVG(Quantity) FROM daily_prayer";
+
+                var avgAllTime = Convert.ToDouble(tableCmd.ExecuteScalar()).ToString("0");
+                Console.WriteLine($"Average for all time: " + avgAllTime + "/5");
+            }
+
+            connection.Close();
+        }
+
 
     }
 }
