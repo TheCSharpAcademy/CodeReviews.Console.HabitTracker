62f4007b43dce1b0f345df5d4e0f4b02f5f47605
Dejmenek <dejmen14567@gmail.com>
2024-02-17 18:34:24 +0100

    Remove unnecessary usings

--- HabitTracker.Dejmenek/HabitTracker.Dejmenek/Models/Habit.cs
+++ HabitTracker.Dejmenek/HabitTracker.Dejmenek/Models/Habit.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HabitTracker.Dejmenek.Models
+﻿namespace HabitTracker.Dejmenek.Models
 {
     public class Habit
     {
