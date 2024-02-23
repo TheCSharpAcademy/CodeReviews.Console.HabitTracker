c223335e5042330a6f832b1a957b50ea740f4ed8
Joshua Abbey <Joshuaabbey2022@gmail.com>
2024-02-23 10:56:39 +0000

    initial commit

---
+++ HabitDatabaseLibrary/HabitDatabaseLibrary.cs
@@ -0,0 +1,236 @@
+﻿using Newtonsoft.Json;
+using System.Data.SQLite;
+using ConsoleTables;
+using System.Reflection.PortableExecutable;
+
+namespace HabitDatabaseLibrary
+{
+    public class Database
+    {
+        JsonWriter writer;
+        public Database()
+        {
+            Directory.CreateDirectory("Logs");
+            string fileName = $"Logs/databaselog_{DateTime.Now.ToString("dd-MM-yyyy_hh-mm-ss_tt")}.json";
+            // log file
+            StreamWriter logFile = File.CreateText(fileName);
+            logFile.AutoFlush = true;
+            writer = new JsonTextWriter(logFile);
+            writer.Formatting = Formatting.Indented;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Database Log");
+            writer.WriteStartArray();
+        }
+
+        void LogError(Exception ex, string habitName = "null", string countIncrement = "null", string currentCount="null")
+        {
+            writer.WriteValue("Failed");
+            writer.WritePropertyName("Habit input");
+            writer.WriteValue(habitName);
+            writer.WritePropertyName("Count Increment");
+            writer.WriteValue(countIncrement);
+            writer.WritePropertyName("Current Count");
+            writer.WriteValue(currentCount);
+            writer.WritePropertyName("Exception Message");
+            writer.WriteValue(ex.Message);
+            writer.WritePropertyName("Stack Trace");
+            writer.WriteValue(ex.StackTrace);
+            writer.WritePropertyName("DateTime");
+            writer.WriteValue(DateTime.Now.ToString("MM/dd/yyyy hh:mm tt"));
+            writer.WriteEndObject();
+        }
+
+        void LogSuccess(string habitName = "null", string countIncrement = "null", string currentCount = "null")
+        {
+            writer.WriteValue("Success");
+            writer.WritePropertyName("Habit input");
+            writer.WriteValue(habitName);
+            writer.WritePropertyName("Count Increment");
+            writer.WriteValue(countIncrement);
+            writer.WritePropertyName("Current Count");
+            writer.WriteValue(currentCount);
+            writer.WritePropertyName("DateTime");
+            writer.WriteValue(DateTime.Now.ToString("MM/dd/yyyy hh:mm tt"));
+            writer.WriteEndObject();
+        }
+        public SQLiteConnection CreateConnection()
+        {
+
+            SQLiteConnection sqlite_conn;
+            sqlite_conn = new SQLiteConnection($"Data Source=database.db;Version=3;New=True;Compress=True;");
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operation");
+            writer.WriteValue("Open Database");
+            writer.WritePropertyName("Result");
+
+            try
+            {
+                sqlite_conn.Open();
+                LogSuccess();
+            }
+
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+
+
+            return sqlite_conn;
+        }
+
+        public void CreateTable(SQLiteConnection conn)
+        {
+            const string habitTableSQL = "CREATE TABLE Habit_Table (HabitName varchar(255), Count int);";
+            SQLiteCommand sqlite_cmd = conn.CreateCommand();
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operation");
+            writer.WriteValue("Create Table");
+            writer.WritePropertyName("Result");
+
+            try
+            {
+                sqlite_cmd.CommandText = habitTableSQL;
+                sqlite_cmd.ExecuteNonQuery();
+                LogSuccess();
+            }
+
+            catch (SQLiteException ex)
+            {
+                LogError(ex);
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+            
+        }
+
+        public void ReadData(SQLiteConnection conn)
+        {
+            Console.Clear();
+
+            SQLiteDataReader reader;
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operation");
+            writer.WriteValue("Read Table");
+            writer.WritePropertyName("Result");
+
+            try
+            {
+                SQLiteCommand sqlite_cmd = conn.CreateCommand();
+                sqlite_cmd.CommandText = "SELECT * FROM Habit_Table;";
+
+                reader = sqlite_cmd.ExecuteReader();
+                var table = new ConsoleTable("Habit Name", "Habit Count");
+
+                while (reader.Read())
+                {
+                    table.AddRow(reader["HabitName"], reader["Count"]);
+                }
+                table.Write();
+
+
+                LogSuccess();
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+        }
+        public void InsertData(SQLiteConnection conn, string habitName)
+        {
+            SQLiteCommand sqlite_cmd = conn.CreateCommand();
+            string insertHabitSQL = $"INSERT INTO Habit_Table (HabitName, Count) VALUES ('{habitName}', 0);";
+            
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operation");
+            writer.WriteValue("Insert Data");
+            writer.WritePropertyName("Result");
+
+            try
+            {
+                sqlite_cmd.CommandText = insertHabitSQL;
+                sqlite_cmd.ExecuteNonQuery();
+                LogSuccess(habitName: habitName);
+            }
+            catch (Exception ex)
+            {
+                LogError(ex, habitName: habitName);
+            }
+        }
+
+        public void DeleteData(SQLiteConnection conn, string habitName)
+        {
+            SQLiteCommand sqlite_cmd = conn.CreateCommand();
+            string deleteHabitSQL = $"DELETE FROM Habit_Table WHERE HabitName='{habitName}';";
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operation");
+            writer.WriteValue("Delete Data");
+            writer.WritePropertyName("Result");
+            try
+            {
+                sqlite_cmd.CommandText = deleteHabitSQL;
+                sqlite_cmd.ExecuteNonQuery();
+                LogSuccess(habitName: habitName);
+            }
+            catch (Exception ex)
+            {
+                LogError(ex, habitName: habitName);
+            }
+        }
+
+        public void UpdateData(SQLiteConnection conn, string habitName, int count)
+        {
+            SQLiteCommand sqlite_cmd = conn.CreateCommand();
+            SQLiteDataReader reader;
+            int currentCount;
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operation");
+            writer.WriteValue("Update Data");
+            writer.WritePropertyName("Result");
+            try
+            {
+                
+                sqlite_cmd.CommandText = $"SELECT Count FROM Habit_Table WHERE HabitName = '{habitName}';";
+                reader = sqlite_cmd.ExecuteReader();
+                reader.Read();
+                currentCount = reader.GetInt32(0);
+                reader.Close();
+                
+                
+                string updateHabitSQL = $"UPDATE Habit_Table SET Count = {currentCount + count} WHERE habitName = '{habitName}'";
+                sqlite_cmd.CommandText = updateHabitSQL;
+                sqlite_cmd.ExecuteNonQuery();
+                LogSuccess(habitName: habitName, currentCount: currentCount.ToString(), countIncrement: count.ToString());
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+        }
+
+        public void CloseConnection(SQLiteConnection conn)
+        {
+
+            writer.WriteStartObject();
+            writer.WritePropertyName("Operation");
+            writer.WriteValue("Close Connection");
+            writer.WritePropertyName("Result");
+            
+            try
+            {
+                conn.Close();
+                LogSuccess();
+            }
+            catch (Exception ex)
+            {
+                LogError(ex);
+            }
+            writer.WriteEndArray();
+            writer.Close();
+        }
+    }
+}
---
+++ HabitTracker/Program.cs
@@ -0,0 +1,168 @@
+﻿using HabitDatabaseLibrary;
+using System.Data.SQLite;
+
+
+namespace HabitTrackerProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database db = new Database();
+            bool exit = false;
+            string? userInput;
+            int optionSelected;
+
+            SQLiteConnection sqlite_conn = db.CreateConnection();    
+            db.CreateTable(sqlite_conn);
+
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("MAIN MENU");
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("\nType 0 to Close Application.");
+                Console.WriteLine("Type 1 to View All Records");
+                Console.WriteLine("Type 2 to Insert Record");
+                Console.WriteLine("Type 3 to Delete Record");
+                Console.WriteLine("Type 4 to Update Record");
+                Console.WriteLine("------------------------------------------");
+                Console.Write("Option: ");
+                userInput = Console.ReadLine();
+                if (userInput == null || userInput == "")
+                {
+                    Console.WriteLine("Error: Menu option cannot be empty.");
+                    continue;
+                }
+                else if (!int.TryParse(userInput, out optionSelected))
+                {
+                    Console.WriteLine("Error: Menu option must be an intergal number.");
+                    continue;
+                }
+                else
+                {
+                    switch (optionSelected)
+                    {
+                        case 0: // Close application
+                            Console.Clear();
+
+                            db.CloseConnection(sqlite_conn);
+
+                            Console.WriteLine("Application will close in 3 seconds.");
+                            Thread.Sleep(3000);
+
+                            exit = true;
+
+                            break;
+                        case 1: // View All Records - view all habits
+
+                            db.ReadData(sqlite_conn);
+
+                            Console.WriteLine("Press any key to continue.");
+                            Console.ReadKey();
+
+                            break;
+                        case 2: // Insert Records - Add a habit
+                            exit = false;
+                            do
+                            {
+                                Console.Clear();
+                                Console.Write("Enter Habit Name (or enter 'e' to return to menu): ");
+                                userInput = Console.ReadLine();
+                                
+                                if (userInput == "" || userInput == null)
+                                {
+                                    continue;
+                                }
+                                
+                                else if (userInput.ToLower() == "e")
+                                    exit = true;
+                                
+                                else
+                                {
+                                    db.InsertData(sqlite_conn, userInput.ToLower());
+                                    exit = true;
+                                }
+
+                            } while (!exit);
+                            exit = false;
+                            
+                            break;
+                        case 3: // Delete Records - Delete a habit
+                            exit = false;
+                            string? confirm;
+                            do
+                            {
+                                Console.Clear();
+                                Console.Write("Name of habit record to be deleted (or enter 'e' to return to menu: ");
+                                userInput = Console.ReadLine();
+                                if (userInput == "" || userInput == null)
+                                    continue;
+
+                                else if (userInput.ToLower() == "e")
+                                {
+                                    exit = true;
+                                    continue;
+                                }
+
+                                Console.Write($"Are you sure you want to delete '{userInput}' (Y/N): ");
+                                confirm = Console.ReadLine();
+
+                                if (confirm == "" || confirm == null)
+                                    continue;
+
+                                else if (confirm.ToLower() == "y")
+                                {
+                                    db.DeleteData(sqlite_conn, userInput.ToLower());
+                                    exit = true;
+                                }
+                                else
+                                    continue;
+                                
+                            }while (!exit);
+                            exit = false;
+                            break;
+                        case 4: // Update Records - Update Habbit count
+                            exit = false;
+                            int countToBeAdded;
+                            string habitName;
+                            do
+                            {
+                                Console.Clear();
+                                Console.Write("Name of habit record to be update (ore enter 'e' to return to menu: ");
+                                userInput = Console.ReadLine();
+                                
+                                if (userInput == "" || userInput == null)
+                                    continue;
+                                
+                                else if (userInput.ToLower() == "e")
+                                {
+                                    exit = true;
+                                    continue;
+                                }
+
+                                habitName = userInput;
+                                Console.Write($"\nHow much would you like to increase {userInput} Count by?: ");
+                                userInput = Console.ReadLine();
+                                if (!int.TryParse(userInput, out countToBeAdded))
+                                {
+                                    Console.WriteLine("Invalid Input. Please Try Again.");
+                                    Console.WriteLine("Returning...");
+                                    Thread.Sleep(1000);
+                                    continue;
+                                }
+                                db.UpdateData(sqlite_conn, habitName, countToBeAdded);
+                                exit = true;
+                            } while (!exit);
+                            exit = false;
+                            break;
+                        default:
+                            Console.WriteLine($"Error: {optionSelected} is not a menu option.");
+                            break;
+                    }
+                }
+            } while (!exit);
+        }
+    }
+}
