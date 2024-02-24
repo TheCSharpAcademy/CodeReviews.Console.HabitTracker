8917902becbbbecd4a1745b9c8724eabb8a7ecf3
Keira Krieger <keira.f.krieger@gmail.com>
2024-02-23 21:04:14 -0800

    Created query method and data manipulation method. Refactored Delete, Update, Insert, and Get all records to use those methods to get rid of redundant code.

--- habitTracker.fennikko/habitTracker.fennikko/RecordManipulation.cs
+++ habitTracker.fennikko/habitTracker.fennikko/RecordManipulation.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Reflection.PortableExecutable;
 using Microsoft.Data.Sqlite;
 
 namespace habitTracker.fennikko;
@@ -69,56 +70,63 @@ public class RecordManipulation
             }
         }
     }
-
-    public static void GetAllRecords()
+    public static List<DrinkingWater> RunQuery(string commandText)
     {
-        Console.Clear();
         using var connection = new SqliteConnection(ConnectionString);
         connection.Open();
         var tableCmd = connection.CreateCommand();
-        tableCmd.CommandText =
-            $"Select * FROM drinking_water";
+        tableCmd.CommandText = commandText;
         List<DrinkingWater> tableData = new();
         var reader = tableCmd.ExecuteReader();
-        if (reader.HasRows)
+        while (reader.Read())
         {
-            while (reader.Read())
+            tableData.Add(
+                new DrinkingWater
+                {
+                    Id = reader.GetInt32(0),
+                    Date = DateTime.ParseExact(reader.GetString(1), "dd-MM-yy", new CultureInfo("en-US")),
+                    Quantity = reader.GetInt32(2)
+                });
+        }
+
+        return tableData;
+    }
+    public static int RunDataManipulation(string commandText)
+    {
+        using var connection = new SqliteConnection(ConnectionString);
+        connection.Open();
+        var tableCmd = connection.CreateCommand();
+        tableCmd.CommandText = commandText;
+
+        return tableCmd.ExecuteNonQuery();
+    }
+    public static void GetAllRecords()
+    {
+        Console.Clear();
+        var tableData = RunQuery($"Select * FROM drinking_water");
+        if (tableData.Count > 0)
+        {
+            Console.WriteLine("--------------------------------");
+
+            foreach (var dw in tableData)
             {
-                tableData.Add(
-                    new DrinkingWater
-                    {
-                        Id = reader.GetInt32(0),
-                        Date = DateTime.ParseExact(reader.GetString(1),"dd-MM-yy", new CultureInfo("en-US")),
-                        Quantity = reader.GetInt32(2)
-                    });
+                Console.WriteLine($"{dw.Id} - {dw.Date:dd-MMM-yyyy} - Quantity: {dw.Quantity}");
             }
         }
         else
         {
             Console.WriteLine("No rows found.");
         }
-        connection.Close();
         Console.WriteLine("--------------------------------");
-        foreach (var dw in tableData)
-        {
-            Console.WriteLine($"{dw.Id} - {dw.Date:dd-MMM-yyyy} - Quantity: {dw.Quantity}");
-        }
-        Console.WriteLine("--------------------------------");
+        
     }
-
     public static void Delete()
     {
         Console.Clear();
         GetAllRecords();
 
         var recordId = GetNumberInput("Please type the Id of the record you want to delete. or type 0 to go back to the main menu");
-
-        using var connection = new SqliteConnection(ConnectionString);
-        connection.Open();
-        var tableCmd = connection.CreateCommand();
-        tableCmd.CommandText = $"DELETE from drinking_water WHERE Id = '{recordId}'";
-
-        var rowCount = tableCmd.ExecuteNonQuery();
+        var rowCount = RunDataManipulation($"DELETE from drinking_water WHERE Id = '{recordId}'");
         if (rowCount == 0)
         {
             Console.WriteLine($"Record with ID {recordId} doesn't exist.");
@@ -128,53 +136,33 @@ public class RecordManipulation
         Console.WriteLine($"Record with ID {recordId} was deleted.");
         Thread.Sleep(3000);
         Console.Clear();
-        tableCmd.ExecuteNonQuery();
-        connection.Close();
+        
     }
-
     public static void Update()
     {
         Console.Clear();
         GetAllRecords();
 
         var recordId = GetNumberInput("Please type the ID of the record you would like to update. Type 0 to return to the main menu.");
-        using var connection = new SqliteConnection( ConnectionString);
-        connection.Open();
-        var checkCmd = connection.CreateCommand();
-        checkCmd.CommandText = $"SELECT EXISTS(SELECT 1 FROM drinking_water WHERE Id = {recordId})";
-        var checkQuery = Convert.ToInt32(checkCmd.ExecuteScalar());
-        if (checkQuery == 0)
+        var updateList = RunQuery($"SELECT * FROM drinking_water WHERE Id = {recordId}");
+        if (updateList.Count == 0)
         {
             Console.WriteLine($"Record with ID {recordId} doesn't exist.");
-            connection.Close();
+            Thread.Sleep(3000);
             Update();
         }
 
         var date = GetDateInput();
         var quantity = GetNumberInput("Please insert number of glasses (no decimals allowed)");
-        var tableCmd = connection.CreateCommand();
-        tableCmd.CommandText =
-            $"UPDATE drinking_water SET date = '{date}', quantity = {quantity} WHERE Id = {recordId}";
-        tableCmd.ExecuteNonQuery();
-        connection.Close();
+        RunDataManipulation($"UPDATE drinking_water SET date = '{date}', quantity = {quantity} WHERE Id = {recordId}");
     }
-
     public static void Insert()
     {
         var date = GetDateInput();
         var quantity = GetNumberInput("Please insert a number of glasses (no decimals allowed)");
-
-        using var connection = new SqliteConnection(ConnectionString);
-        connection.Open();
-        var tableCmd = connection.CreateCommand();
-
-        tableCmd.CommandText =
-            $"INSERT INTO drinking_water(date, quantity) VALUES('{date}', {quantity})";
-        tableCmd.ExecuteNonQuery();
-        connection.Close();
+        RunDataManipulation($"INSERT INTO drinking_water(date, quantity) VALUES('{date}', {quantity})");
         Console.Clear();
     }
-
     public static string GetDateInput()
     {
         Console.WriteLine("Please insert the date: (Format: dd-mm-yy). Type 0 to return to the main menu");
@@ -188,7 +176,6 @@ public class RecordManipulation
         }
         return dateInput;
     }
-
     public static int GetNumberInput(string? message)
     {
         Console.WriteLine(message);
