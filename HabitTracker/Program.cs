58d180a859b5e51b960d90ea79ce9dab176728af
Emgigas <119613722+Emgigas@users.noreply.github.com>
2024-03-24 18:57:25 +0000

    Create DisplayMenu method

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,1 +1,13 @@
-﻿// test Git working
+﻿DisplayMenu();
+
+void DisplayMenu()
+{
+    Console.WriteLine("MAIN MENU\n");
+    Console.WriteLine("What would you like to do?\n");
+    Console.WriteLine("Type 0 to Close the Application.");
+    Console.WriteLine("Type 1 to View All Records.");
+    Console.WriteLine("Type 2 to Insert a Record.");
+    Console.WriteLine("Type 3 to Update a Record.");
+    Console.WriteLine("Type 4 to Delete a Record.");
+    Console.WriteLine("--------------------------------\n");
+}
