73f0e49085f2b57f4223e9316deedb7a910d1b83
moth <dinocruz434@gmail.com>
2024-04-01 15:07:10 +0100

    Client Basico ja criado

---
+++ Habit-Tracker.Logic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Data.SQLite;
+
+namespace Mothnue.Program
+{
+    class Logic
+    {
+        public string CreateDatabase()
+        {
+            const string databaseName = "myDatabase.db";
+            if (!File.Exists(databaseName))
+            {
+                SQLiteConnection.CreateFile(databaseName);
+                return "Database Created!";
+            }
+            else
+            {
+                return "Database already exist!";
+            }
+        }
+    }
+}
---
+++ Habit-Tracker.Mothnue/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Mothnue.Program
+{
+    class Client
+    {
+        private static void Main()
+        {
+            Client menu = new Client();
+            bool application = true;
+
+            while (application)
+            {
+                int response = menu.MainMenu();
+                switch (response)
+                {
+                    case 0:
+                        application = false;
+                        break;
+                    case 1:
+                        // View all Records
+                        break;
+                    case 3:
+                        // Delete a Record
+                        break;
+                    case 4:
+                        // Update a Record
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        private int MainMenu()
+        {
+            Console.WriteLine("MAIN MENU");
+            Console.WriteLine("What would you like to do ?");
+
+            Console.WriteLine("0 - Close");
+            Console.WriteLine("1 - View all Records");
+            Console.WriteLine("3 - Delete a Record");
+            Console.WriteLine("4 - Update a Record");
+
+            int userInput;
+            while (!int.TryParse(Console.ReadLine(), out userInput))
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+
+            return userInput;
+        }
+    }
+}
