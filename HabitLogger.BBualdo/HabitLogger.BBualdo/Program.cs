be9a5755e01db30e01de4147ae466c0591ecaae7
Sebastian <mrseb10@gmail.com>
2024-04-09 01:55:38 +0200

    Project Finished

---
+++ HabitLogger.BBualdo/HabitLogger.BBualdo/Program.cs
@@ -0,0 +1,34 @@
+﻿using HabitLogger.BBualdo;
+using HabitLogger.BBualdo.Database;
+
+DbContext db = new DbContext();
+
+bool isAppClosed = false;
+
+while (!isAppClosed)
+{
+  MainMenu.ShowMenu();
+
+  string userInput = MainMenu.GetUserInput();
+
+  switch (userInput)
+  {
+    case "0":
+      Console.WriteLine("\nSee you soon!\n");
+      isAppClosed = true;
+      break;
+    case "1":
+      db.GetAllData();
+      Console.WriteLine("\nPress any key to return to Main Menu.");
+      Console.ReadKey();
+      break;
+    case "2":
+      db.InsertData(); break;
+    case "3":
+      db.DeleteData(); break;
+    case "4":
+      db.UpdateData(); break;
+    default:
+      break;
+  }
+}
