4422fb8d25bf0c3d17c8518616cc41da7cee0dcf
Arashi256 <arashi256@hotmail.com>
2024-04-14 14:23:29 +0100

    Codacy fixes 4

--- HabitLogger.Arashi256/Program.cs
+++ HabitLogger.Arashi256/Program.cs
@@ -762,8 +762,6 @@ public class Habit
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Units { get; set; }
-
-    // Static method to create a new instance of Habit
     public static Habit CreateNewHabit(int id, string name, string units)
     {
         return new Habit
