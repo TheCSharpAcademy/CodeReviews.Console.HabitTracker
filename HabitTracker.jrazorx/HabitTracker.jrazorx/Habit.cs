3ca1bf81d41bb11f95066d4a2e14a46f2e2a55e1
jra <julien.gault.50@gmail.com>
2024-06-19 16:21:44 +0200

    initial commit with project skeleton

---
+++ HabitTracker.jrazorx/HabitTracker.jrazorx/Habit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HabitTracker
+{
+    public class Habit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ HabitTracker.jrazorx/HabitTracker.jrazorx/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HabitTracker
+{
+    class Program
+    {
+        private static string connectionString = "Data Source=habit_tracker.db";
+
+        static void Main(string[] args)
+        {
+            HabitTrackerApp app = new HabitTrackerApp(connectionString);
+            app.Run();
+        }
+    }
+}
