fd53bf93f57554fdd70f20ff8b3e42f3f9e52fb0
jra <julien.gault.50@gmail.com>
2024-06-19 17:13:35 +0200

    Menu

--- HabitTracker.jrazorx/HabitTracker.jrazorx/HabitTrackerApp.cs
+++ HabitTracker.jrazorx/HabitTracker.jrazorx/HabitTrackerApp.cs
@@ -2,13 +2,55 @@
 
 namespace HabitTracker
 {
-    public class HabitTrackerApp
+    public class HabitTrackerApp(string connectionString)
     {
-        private readonly HabitRepository habitRepository;
+        private readonly HabitRepository habitRepository = new(connectionString);
 
-        public HabitTrackerApp(string connectionString)
+        public static void Run()
         {
-            habitRepository = new HabitRepository(connectionString);
+            while (true)
+            {
+                ShowMenu();
+                if (!int.TryParse(Console.ReadLine(), out int choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        //InsertHabit();
+                        break;
+                    case 2:
+                        //ViewHabits();
+                        break;
+                    case 3:
+                        //UpdateHabit();
+                        break;
+                    case 4:
+                        //DeleteHabit();
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        private static void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("\nHabit Tracker Menu:");
+            Console.WriteLine("1. Insert Habit");
+            Console.WriteLine("2. View Habits");
+            Console.WriteLine("3. Update Habit");
+            Console.WriteLine("4. Delete Habit");
+            Console.WriteLine("0. Exit");
+            Console.Write("Enter your choice: ");
         }
     }
 }
