0ad1c71c22711b4f3825559146496e4b3ad5883c
Alvin Graham <grah0071@gmail.com>
2024-06-25 20:19:20 -0500

    Create DB in C#

--- HabitTracker.AlvinGraham/HabitTracker.AlvinGraham/Program.cs
+++ HabitTracker.AlvinGraham/HabitTracker.AlvinGraham/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habit-Tracker.db";
+
+CreateDatabase();
+
+void CreateDatabase()
+{
+	using (SqliteConnection connection = new(connectionString))
+	{
+		using (SqliteCommand tableCmd = connection.CreateCommand())
+		{
+			connection.Open();
+			tableCmd.CommandText =
+				@"CREATE TABLE IF NOT EXISTS walkingHabit (
+					Id INTEGER PRIMARY KEY AUTOINCREMENT,
+					Date TEXT,
+					Quantity INTEGER
+					)";
+			tableCmd.ExecuteNonQuery();
+		}
+	}
+}
