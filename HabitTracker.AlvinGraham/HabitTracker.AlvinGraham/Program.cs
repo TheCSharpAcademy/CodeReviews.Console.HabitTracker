3bac57174f828c8de0828f2806ef257176a6b440
Alvin Graham <grah0071@gmail.com>
2024-06-25 20:34:59 -0500

    Create user menu

--- HabitTracker.AlvinGraham/HabitTracker.AlvinGraham/Program.cs
+++ HabitTracker.AlvinGraham/HabitTracker.AlvinGraham/Program.cs
@@ -1,8 +1,53 @@
 ﻿using Microsoft.Data.Sqlite;
+using Spectre.Console;
 
 string connectionString = @"Data Source=habit-Tracker.db";
 
 CreateDatabase();
+
+MainMenu();
+
+void MainMenu()
+{
+	var isMenuRunning = true;
+
+	while (isMenuRunning)
+	{
+		var userChoice = AnsiConsole.Prompt(
+			new SelectionPrompt<string>()
+			.Title("What would you like to do?")
+			.AddChoices(
+				"Add Record",
+				"Delete Record",
+				"View Records",
+				"Update Record",
+				"Quit")
+			);
+
+		switch (userChoice)
+		{
+			case "Add Record":
+				//AddRecord();
+				break;
+			case "Delete Record":
+				//DeleteRecord();
+				break;
+			case "View Records":
+				//ViewRecords();
+				break;
+			case "Update Record":
+				//UpdateRecord();
+				break;
+			case "Quit":
+				Console.WriteLine("Goodbye!");
+				isMenuRunning = false;
+				break;
+			default:
+				Console.WriteLine("Invalid Choice. Please choose one of the above");
+				break;
+		}
+	}
+}
 
 void CreateDatabase()
 {
