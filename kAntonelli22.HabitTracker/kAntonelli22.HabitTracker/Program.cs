b9743931ced905f2b4bb04533bdddfb1d590c380
Kiernan Antonelli <fourthantonelli@gmail.com>
2024-07-02 17:13:54 -0400

    Fixed remove and modify errors and code cleanup.

--- kAntonelli22.HabitTracker/kAntonelli22.HabitTracker/Program.cs
+++ kAntonelli22.HabitTracker/kAntonelli22.HabitTracker/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Data.SQLite;
-using System.Reflection.PortableExecutable;
 
 class Program
 {
@@ -14,7 +13,7 @@ class Program
                 Quantity TEXT NOT NULL
             );";
 
-        CreateTable(query);
+        RunQuery(query);
         MainMenu();
     }
 
@@ -27,7 +26,7 @@ class Program
     1. View Records
     2. Log Record
     3. Modify Record
-    4. Remove Records
+    4. Remove Record
     5. Add Habit
     6. Remove Habit
     7. Modify Habit
@@ -65,32 +64,23 @@ class Program
         Console.WriteLine("What is the quantity");
         string quantity = CleanResponse(Console.ReadLine());
 
-        ModifyDB($"INSERT INTO {viewRecords.Item1} (Date, Quantity) VALUES ('{date}','{quantity}');");
+        RunQuery($"INSERT INTO {viewRecords.Item1} (Date, Quantity) VALUES ('{date}','{quantity}');");
     } // end of LogRecord method
 
     static void ChangeRecord()
     {
+        Tuple<string, List<string>, List<string>> viewRecords = ViewRecords();
+        Console.WriteLine("Which Record would you like to modify?");
+        int response = GetValidResponse(viewRecords.Item2.Count);
 
         Console.WriteLine("What is the records date?");
         string date = CleanResponse(Console.ReadLine());
         Console.WriteLine("What is the quantity");
         string quantity = CleanResponse(Console.ReadLine());
 
-        Tuple<string, List<string>, List<string>> viewRecords = ViewRecords();
-
-        Console.WriteLine("Which Record would you like to modify?");
-        int response;
-        Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
-
-        while (!(response > 0) || !(response <= viewRecords.Item2.Count))
-        {
-            Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
-            Console.WriteLine("enter the number of one of the listed habits or menu to return to menu");
-        }
-
         string query = $"SELECT Id FROM {viewRecords.Item1} LIMIT 1 OFFSET {response - 1}";
         int id = GetID(query);
-        ModifyDB($"UPDATE {viewRecords.Item1} SET Date = '{date}', Quantity = '{quantity}' WHERE Id = {id}");
+        RunQuery($"UPDATE {viewRecords.Item1} SET Date = '{date}', Quantity = '{quantity}' WHERE Id = {id}");
     } // end of ChangeRecord method
 
     static void RemoveRecord()
@@ -98,38 +88,24 @@ class Program
         Tuple<string, List<string>, List<string>> viewRecords = ViewRecords();
 
         Console.WriteLine("Which Record would you like to remove?");
-        int response;
-        Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
-
-        while (!(response > 0) || !(response <= viewRecords.Item2.Count))
-        {
-            Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
-            Console.WriteLine("enter the number of one of the listed habits or menu to return to menu");
-        }
+        int response = GetValidResponse(viewRecords.Item2.Count);
 
         int id = GetID($"SELECT Id FROM {viewRecords.Item1} LIMIT 1 OFFSET {response - 1}");
-        ModifyDB($"DELETE FROM {viewRecords.Item1} WHERE Id = {id}");
+        RunQuery($"DELETE FROM {viewRecords.Item1} WHERE Id = {id}");
     } // end of RemoveRecord method
 
     static Tuple<string, List<string>, List<string>> ViewRecords()
     {
         Console.WriteLine("Which Habit would you like to view?");
-        List<string> Habits = DisplayHabits();
-        int response;
-        Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
-
-        string habit;
-        while (!(response > 0) || !(response <= Habits.Count))
-        {
-            Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
-            Console.WriteLine("enter the number of one of the listed habits or menu to return to menu");
-        }
-        habit = Habits[response - 1];
+        Tuple<List<string>, List<string>> habitsTable = DisplayHabits();
+        int response = GetValidResponse(habitsTable.Item1.Count);
+        string habit = habitsTable.Item1[response - 1];
+        string measurement = habitsTable.Item2[response - 1];
 
         Tuple<List<string>, List<string>> results = SelectData("SELECT * FROM " + habit);
-        for (int i = 1; i < results.Item1.Count; i++)
+        for (int i = 0; i < results.Item1.Count; i++)
         {
-            Console.WriteLine($"{i}. {results.Item1[i].PadRight(10)} {results.Item2[i].PadLeft(10)}");
+            Console.WriteLine($"{i + 1}. {results.Item1[i].PadRight(10)} {$"{results.Item2[i]} {measurement}".PadLeft(10)}");
         }
         return new Tuple<string, List<string>, List<string>>(habit, results.Item1, results.Item2);
     } // end of ViewRecords method
@@ -148,65 +124,46 @@ class Program
                 Date TEXT NOT NULL,
                 Quantity TEXT NOT NULL
             );";
-        CreateTable(query);
+        RunQuery(query);
 
-        ModifyDB($"INSERT INTO Habits (Habit, Quantity) VALUES ('{name}','{quantity}');");
+        RunQuery($"INSERT INTO Habits (Habit, Quantity) VALUES ('{name}','{quantity}');");
     } // end of AddRecord method
 
     static void RemoveHabit()
     {
         Console.WriteLine("Which Habit would you like to remove?");
-        List<string> Habits = DisplayHabits();
-        int response;
-        Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
-
-        while (!(response > 0) || !(response <= Habits.Count))
-        {
-            Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
-            Console.WriteLine("enter the number of one of the listed habits or menu to return to menu");
-        }
+        Tuple<List<string>, List<string>> habitsTable = DisplayHabits();
+        int response = GetValidResponse(habitsTable.Item1.Count);
 
         int id = GetID($"SELECT Id FROM Habits LIMIT 1 OFFSET {response - 1}");
-        Console.WriteLine($"id: {id}");
-        ModifyDB($"DELETE FROM Habits WHERE Id = {id}");
-
-        // find and remove the habits entry in the habit table and delete its records
+        RunQuery($"DELETE FROM Habits WHERE Id = {id}");
+        RunQuery($"DROP TABLE IF EXISTS {habitsTable.Item1[response - 1]}");
     } // end of RemoveRecord method
 
     static void ModifyHabit()
     {
+        Console.WriteLine("Which Habit would you like to modify?");
+        Tuple<List<string>, List<string>> habitsTable = DisplayHabits();
+        int response = GetValidResponse(habitsTable.Item1.Count);
+
         Console.WriteLine("What is the name of the Habit?");
         string name = CleanResponse(Console.ReadLine());
         Console.WriteLine("How is it measured? ex: mph");
         string quantity = CleanResponse(Console.ReadLine());
 
-        Console.WriteLine("Which Habit would you like to modify?");
-        List<string> Habits = DisplayHabits();
-        int response;
-        Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
-
-        while (!(response > 0) || !(response <= Habits.Count))
-        {
-            Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
-            Console.WriteLine("enter the number of one of the listed habits or menu to return to menu");
-        }
-
         int id = GetID($"SELECT Id FROM Habits LIMIT 1 OFFSET {response - 1}");
-        Console.WriteLine($"id: {id}");
-        ModifyDB($"UPDATE Habits SET Habit = '{name}', Quantity = '{quantity}' WHERE Id = {id};");
+        RunQuery($"UPDATE Habits SET Habit = '{name}', Quantity = '{quantity}' WHERE Id = {id};");
     } // end of ViewRecords method
 
-
-
-    static List<string> DisplayHabits()
+    static Tuple<List<string>, List<string>> DisplayHabits()
     {
         List<string> Habits = new List<string>();
+        List<string> Measurements = new List<string>();
         using (var connection = new SQLiteConnection(connectionString))
         {
             connection.Open();
 
-            string query = "SELECT * FROM Habits";
-            using (var command = new SQLiteCommand(query, connection))
+            using (var command = new SQLiteCommand("SELECT * FROM Habits", connection))
             {
                 using (SQLiteDataReader reader = command.ExecuteReader())
                 {
@@ -215,14 +172,14 @@ class Program
                     {
                         Console.WriteLine($"{lineNumber}. {reader.GetString(1).PadRight(10)} {reader.GetString(2).PadLeft(10)}");
                         Habits.Add(reader.GetString(1));
+                        Measurements.Add(reader.GetString(2));
                         lineNumber++;
                     }
                 }
             }
         }
-        return Habits;
-    }
-
+        return new Tuple<List<string>, List<string>>(Habits, Measurements);
+    } // end of DisplayHabits method
 
     static string CleanResponse(string? response)
     {
@@ -231,7 +188,6 @@ class Program
             return "";
 
         string cleanResponse = response.Trim().ToLower();
-
         if (cleanResponse == "0")
             Environment.Exit(0);
         else if (cleanResponse == "menu")
@@ -240,26 +196,47 @@ class Program
         return cleanResponse;
     } // end of CleanResponse method
 
+    static int GetValidResponse(int max)
+    {
+        int response;
+        Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
+        while (!(response > 0) || !(response <= max))
+        {
+            Int32.TryParse(CleanResponse(Console.ReadLine()), out response);
+            Console.WriteLine("enter the number of one of the listed options or menu to return to the menu");
+        }
+        return response;
+    } // end of GetValidResponse method
+
     // database methods
     static int GetID(string query)
     {
-        int id = -1;
-        using (var connection = new SQLiteConnection(connectionString))
-        {
-            connection.Open();
 
-            using (var command = new SQLiteCommand(query, connection))
+        int id = -1;
+        try
+        {
+            using (var connection = new SQLiteConnection(connectionString))
             {
-                using (SQLiteDataReader reader = command.ExecuteReader())
+                connection.Open();
+
+                using (var command = new SQLiteCommand(query, connection))
                 {
-                    if(reader.Read())
-                        id = reader.GetInt32(0);
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                            id = reader.GetInt32(0);
+                    }
                 }
             }
         }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error has occured: {ex.Message}");
+        }
         return id;
-    }
-    static void CreateTable(string query)
+    } // end of GetID method
+
+    static void RunQuery(string query)
     {
         try
         {
@@ -279,47 +256,35 @@ class Program
         {
             Console.WriteLine($"An error has occured: {ex.Message}");
         }
-    }
+    } // end of RunQuery method
 
     static Tuple<List<string>, List<string>> SelectData(string query)
     {
         List<string> results1 = new List<string>();
         List<string> results2 = new List<string>();
-        using (var connection = new SQLiteConnection(connectionString))
+        try
         {
-            connection.Open();
-
-            using (var command = new SQLiteCommand(query, connection))
+            using (var connection = new SQLiteConnection(connectionString))
             {
-                using (SQLiteDataReader reader = command.ExecuteReader())
+                connection.Open();
+
+                using (var command = new SQLiteCommand(query, connection))
                 {
-                    while (reader.Read())
+                    using (SQLiteDataReader reader = command.ExecuteReader())
                     {
-                        results1.Add(reader.GetString(1));
-                        results2.Add(reader.GetString(1));
+                        while (reader.Read())
+                        {
+                            results1.Add(reader.GetString(1));
+                            results2.Add(reader.GetString(2));
+                        }
                     }
                 }
             }
         }
-        return new Tuple<List<string>, List<string>>(results1, results2);
-    }
-
-    static void ModifyDB(string query)
-    {
-        using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+        catch(Exception ex)
         {
-            connection.Open();
-            SQLiteCommand command = new SQLiteCommand(query, connection);
-            try
-            {
-                command.ExecuteNonQuery();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error has occured: {ex.Message}");
-            }
-            connection.Close();
+            Console.WriteLine($"An error has occured: {ex.Message}");
         }
-
-    }
-}
+        return new Tuple<List<string>, List<string>>(results1, results2);
+    } // end of SelectData method
+} // end of program
