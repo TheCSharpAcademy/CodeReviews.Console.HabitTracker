62e054fbe96f4e782afdcd5eb6a2fe86493a74ef
Batuhan <anhtubui574@gmail.com>
2024-07-03 12:05:37 +0700

    Add ColoredConsole class & update projects
    - Implemented a new `ColoredConsole` class with methods for colored text output and user prompts.

---
+++ HabitLogger.batus3010/ColoredConsole/ColoredConsole.cs
@@ -0,0 +1,32 @@
+﻿namespace ColoredConsole
+{
+    public class ColoredConsole
+    {
+        public static void WriteLine(string text, ConsoleColor color)
+        {
+            ConsoleColor previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = previousColor;
+        }
+
+        public static void Write(string text, ConsoleColor color)
+        {
+            ConsoleColor previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(text);
+            Console.ForegroundColor = previousColor;
+        }
+
+        public static string Prompt(string questionToAsk = "")
+        {
+            ConsoleColor previousColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(questionToAsk + " ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string input = Console.ReadLine() ?? ""; // If we got null, use empty string instead.
+            Console.ForegroundColor = previousColor;
+            return input;
+        }
+    }
+}
