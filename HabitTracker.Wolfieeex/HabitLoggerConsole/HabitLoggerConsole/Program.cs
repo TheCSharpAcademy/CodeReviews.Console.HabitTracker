e342bd59cd1e50faa208aecdd240f909bc697ffa
Wolfieeex <Wolfieeex400@users.noreply.github.com>
2024-07-30 21:00:13 +0100

    Insert method complete

--- HabitTracker.Wolfieeex/HabitLoggerConsole/HabitLoggerConsole/Program.cs
+++ HabitTracker.Wolfieeex/HabitLoggerConsole/HabitLoggerConsole/Program.cs
@@ -1,28 +1,130 @@
 ﻿using Microsoft.Data.Sqlite;
+using HabitLoggerConsole;
 using System;
 
 namespace HabitLoggerConsole;
 
 public class Program
 {
+    public static string connectionString { private set; get; } = @"Data Source=HabitLoggerConsole.db";
+
+
     static void Main(string[] args)
     {
-        string connectionString = @"Data Source=HabitLoggerConsole.db";
-
-        using (var connection = new SqliteConnection(connectionString))
+        try
         {
-            connection.Open();
-            var tableCmd = connection.CreateCommand();
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
 
-            tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS going_to_gym (
+                tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS going_to_gym (
                                         Id INTEGER PRIMARY KEY AUTOINCREMENT,
                                         Date TEXT,
                                         Sets TEXT
                                         )";
 
-            tableCmd.ExecuteNonQuery();
+                tableCmd.ExecuteNonQuery();
 
-            connection.Close();
+                connection.Close();
+            }
+
+            MainMenu();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+
+    static void MainMenu()
+    {
+        bool closeApp = false;
+        while (closeApp == false)
+        {
+            Console.Clear();
+
+            Console.WriteLine("MAIN MENU:");
+            Console.WriteLine("\nWhat would you like to do?");
+            Console.WriteLine($"{new string('-', Console.BufferWidth)}");
+            Console.WriteLine("\n0 - Close application");
+            Console.WriteLine("1 - View all records");
+            Console.WriteLine("2 - Insert a record");
+            Console.WriteLine("3 - Delete a record");
+            Console.WriteLine("4 - Update a record\n");
+            Console.WriteLine($"{new string('-', Console.BufferWidth)}");
+            Console.Write("\nYour input: ");
+
+            int userInput = -1;
+            AssignSelectionInput(ref userInput, 0, 4);
+
+            switch (userInput)
+            {
+                case 0:
+                    Console.Clear();
+                    Console.WriteLine("Thank you for using this application. See you soon!\n");
+                    closeApp = true;
+                    break;
+                case 1:
+                    Console.Clear();
+                    SqlCommands.GetAllRecords();
+                    break;
+                case 2:
+                    Console.Clear();
+                    SqlCommands.Insert();
+                    break;
+                case 3:
+                    Console.Clear();
+                    SqlCommands.Delete();
+                    break;
+                case 4:
+                    Console.Clear();
+                    SqlCommands.Update();
+                    break;
+            }
+        }
+    }
+
+    static void AssignSelectionInput(ref int input, int rangeMin, int rangeMax)
+    {
+        bool inputChecksNotComplete = true;
+        while (inputChecksNotComplete)
+        {
+            string? Input = Console.ReadLine();
+            string reason = "";
+
+            if (string.IsNullOrEmpty(Input))
+            {
+                ReenterLine("Your input is either invalid or empty.");
+                continue;
+            }
+
+            int numericOutput = -1;
+            if (!int.TryParse(Input, out numericOutput))
+            {
+                ReenterLine("Your input must be a valid integer number.");
+                continue;
+            }
+
+            else
+            {
+                if (numericOutput < rangeMin || numericOutput > rangeMax)
+                {
+                    ReenterLine($"Your input must be a number ranging from {rangeMin} to {rangeMax}.");
+                    continue;
+                }
+            }
+
+            input = numericOutput;
+            inputChecksNotComplete = false;
+        }
+
+        void ReenterLine(string reason)
+        {
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.WriteLine($"{new string(' ', Console.BufferWidth)}");
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
+            Console.Write(reason + " Please try again to select your option: ");
         }
     }
 }
--- HabitTracker.Wolfieeex/HabitLoggerConsole/HabitLoggerConsole/SqlCommands.cs
+++ HabitTracker.Wolfieeex/HabitLoggerConsole/HabitLoggerConsole/SqlCommands.cs
@@ -1,6 +1,53 @@
-﻿namespace HabitLoggerConsole
+﻿using Microsoft.Data.Sqlite;
+using HabitLoggerConsole;
+
+namespace HabitLoggerConsole;
+
+internal class SqlCommands
 {
-    internal class SqlCommands
+    internal static void GetAllRecords()
     {
+        throw new NotSupportedException();
+    }
+    internal static void Insert()
+    {
+        string date = GetDateInput();
+
+        // Add functionality for user's input for number of sets performed this day with numeric data validation:
+
+        Random random = new Random();
+        int numberOfSets = random.Next(31);
+
+        using (var connection = new SqliteConnection(Program.connectionString))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+
+            tableCmd.CommandText = $"INSERT INTO going_to_gym(Date, Sets) VALUES('{date}', '{numberOfSets}')";
+            tableCmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+    }
+    internal static void Delete()
+    {
+        throw new NotSupportedException();
+    }
+    internal static void Update()
+    {
+        throw new NotSupportedException();
+    }
+
+    private static string GetDateInput()
+    {
+        Console.WriteLine("Please insert the date of the operation (Format that is accepted: dd-mm-yyyy");
+        Console.WriteLine("Or, select 0 and press enter to return to Main Menu.");
+        Console.Write("\nYour input: ");
+
+        string? dateInput = Console.ReadLine();
+
+        // Add data validation for date format presented and availability to enter 0 to discontinue this operation.
+
+        return dateInput;
     }
 }
