68ada4f35c0813cdb8becbda052359043b347d44
Joe <joe980@gmail.com>
2024-08-06 20:46:34 +0100

    Added project for review

---
+++ HabitTracker.JoeW980/Database.cs
@@ -0,0 +1,197 @@
+﻿using System.Data.SQLite;
+
+namespace HabitLogger;
+
+internal class Database
+{
+    private readonly string _fileName = "Habit-Logger.db";
+    private string _connectionString = "DataSource=Habit-Logger.db";
+    public SQLiteConnection DbConnection = new SQLiteConnection();
+    private Random _rnd = new Random();
+
+    public Database()
+    {
+        DbConnection = new SQLiteConnection(_connectionString);
+        DbConnection.Open();
+
+        if(!File.Exists(_fileName) || new FileInfo(_fileName).Length == 0 || GetHabits().Count < 1)
+            CreateTablesIfNotExist();
+    }
+
+    public void CreateTablesIfNotExist()
+    {
+        using var cmd = new SQLiteCommand(DbConnection);
+
+        //Habits Table
+        cmd.CommandText = "DROP TABLE IF EXISTS Habits";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "CREATE TABLE Habits(id INTEGER PRIMARY KEY, Name TEXT, Unit TEXT)";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = @"INSERT INTO Habits(Name, Unit) VALUES ('Water', 'Glasses'), ('Fruit', 'Portions'), ('Running', 'Miles')";
+        cmd.ExecuteNonQuery();
+        Console.WriteLine("Habits Table Created");
+
+        //Water Table
+        cmd.CommandText = "DROP TABLE IF EXISTS Water";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "CREATE TABLE Water(id INTEGER PRIMARY KEY, Quantity INTEGER, Datetime DATE)";
+        cmd.ExecuteNonQuery();
+        for (int i = 0; i < 100; i++)
+        {
+            cmd.CommandText = $"INSERT INTO Water(Quantity, DateTime) VALUES({_rnd.Next(1,8)},'{DateTime.UtcNow.AddDays(-100 + i + _rnd.Next(1, 3))}')";
+            cmd.ExecuteNonQuery();
+        }        
+        Console.WriteLine("Water Table Created");
+
+        //Fruit Table
+        cmd.CommandText = "DROP TABLE IF EXISTS Fruit";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "CREATE TABLE Fruit(id INTEGER PRIMARY KEY, Quantity INTEGER, Datetime DATE)";
+        cmd.ExecuteNonQuery();
+        for (int i = 0; i < 100; i++)
+        {
+            cmd.CommandText = $"INSERT INTO Fruit(Quantity, DateTime) VALUES({_rnd.Next(1, 10)},'{DateTime.UtcNow.AddDays(-100 + i + _rnd.Next(1, 3))}')";
+            cmd.ExecuteNonQuery();
+        }        
+        Console.WriteLine("Fruit Table Created");
+
+        //Running Table
+        cmd.CommandText = "DROP TABLE IF EXISTS Running";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = "CREATE TABLE Running(id INTEGER PRIMARY KEY, Quantity INTEGER, Datetime DATE)";
+        cmd.ExecuteNonQuery();
+        for (int i = 0; i < 100; i++)
+        {
+            cmd.CommandText = $"INSERT INTO Running(Quantity, DateTime) VALUES({_rnd.Next(3, 10)},'{DateTime.UtcNow.AddDays(-100 + i + _rnd.Next(1, 3))}')";
+            cmd.ExecuteNonQuery();
+        }
+        Console.WriteLine("Running Table Created");
+        cmd.Dispose();
+    }
+
+    public void AddNewHabit(string HabitName, string Unit)
+    {
+        using var cmd = new SQLiteCommand(DbConnection);
+
+        //Create table for habit
+        cmd.CommandText = $"DROP TABLE IF EXISTS {HabitName}";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = $"CREATE TABLE {HabitName}(id INTEGER PRIMARY KEY, Quantity INTEGER, Datetime DATE)";
+        cmd.ExecuteNonQuery();
+
+        //Add Habit into habits table
+        cmd.CommandText = $"DELETE FROM Habits Where Name =  '{HabitName}'";
+        cmd.ExecuteNonQuery();
+        cmd.CommandText = $"INSERT INTO Habits(Name, Unit) VALUES ('{HabitName}', '{Unit}')";
+        cmd.ExecuteNonQuery();
+        cmd.Dispose ();
+    }
+
+    public List<Habit> GetHabits()
+    {
+        var habits = new List<Habit>();
+        string getHabits = "SELECT id, Name, Unit FROM Habits";
+        using var cmd = new SQLiteCommand(getHabits, DbConnection);
+        using SQLiteDataReader rdr = cmd.ExecuteReader();
+
+        while (rdr.Read())
+        {
+            habits.Add(new Habit(rdr.GetInt32(0), rdr.GetString(1), rdr.GetString(2), new List<Entry>()));
+        }
+        rdr.Close();
+        cmd.Dispose();
+
+        return habits;
+    }
+
+    public List<Entry> GetHabitRecords(Habit habit)
+    {
+        string getDetails = "SELECT id, Quantity, Datetime FROM " + habit.HabitName;
+        using var cmd = new SQLiteCommand(getDetails, DbConnection);
+        using SQLiteDataReader rdr = cmd.ExecuteReader();
+        while (rdr.Read())
+        {
+            DateTime.TryParse(rdr.GetString(2), out var date);
+            habit.Entries.Add(new Entry(rdr.GetInt32(0), rdr.GetInt32(1), date));
+        }
+        rdr.Close();
+        cmd.Dispose();
+
+        return habit.Entries;
+    }
+
+    public List<Habit> ViewAllRecords()
+    {
+        var habitTables = GetHabits();
+        foreach (Habit habit in habitTables)
+        {
+            GetHabitRecords(habit);
+        }
+        return habitTables;
+    }
+
+    public Report GetReport(Habit habit, string TimeFrame)
+    {        
+        var allEntries = GetHabitRecords(habit);
+        DateTime reportTime = TimeFrame switch
+        {
+            "Yearly" => DateTime.UtcNow.AddYears(-1),
+            "Monthly" => DateTime.UtcNow.AddMonths(-1),
+            "Weekly" => DateTime.UtcNow.AddDays(-7),
+             _ => DateTime.UtcNow.AddDays(-1)
+        };
+        
+        int totalQuantity = 0;
+        int totalRecords = 0;
+        foreach (var entry in allEntries)
+        {
+            if (reportTime <= entry.Date)
+            {
+                totalQuantity += entry.Quantity;
+                totalRecords++;
+            }
+        }
+        
+
+        return new Report(totalQuantity, totalRecords);
+    }
+
+    public void insert(Habit habit, int quantity)
+    {
+        using var cmd = new SQLiteCommand(DbConnection);
+        cmd.CommandText = $"INSERT INTO {habit.HabitName}(Quantity, Datetime) VALUES ({quantity}, '{DateTime.UtcNow}')";                      
+        cmd.ExecuteNonQuery();
+        cmd.Dispose();
+    }
+
+    public void Update(Habit habit, Entry record, int Quantity)
+    {
+        using var cmd = new SQLiteCommand(DbConnection);
+        cmd.CommandText = $"UPDATE {habit.HabitName} SET Quantity = {Quantity}, Datetime = '{DateTime.UtcNow}' WHERE id = {record.id}";
+        cmd.ExecuteNonQuery();
+        cmd.Dispose();
+    }
+
+    public void DeleteHabit(Habit habit)
+    {
+        using var cmd = new SQLiteCommand(DbConnection);
+        cmd.CommandText = $"DELETE FROM Habits Where Name =  '{habit.HabitName}'";
+        cmd.ExecuteNonQuery();
+
+        cmd.CommandText = $"DROP TABLE IF EXISTS {habit.HabitName}";
+        cmd.ExecuteNonQuery();
+        cmd.Dispose();
+    }
+
+    public void DeleteRecord(Habit habit, Entry entry)
+    {
+        using var cmd = new SQLiteCommand(DbConnection);
+        cmd.CommandText = $"DELETE FROM {habit.HabitName} Where id = '{entry.id}'";
+        cmd.ExecuteNonQuery();
+        cmd.Dispose();
+    }
+
+    public record Habit (int id, string HabitName, string Unit, List<Entry> Entries);
+    public record Entry(int id, int Quantity, DateTime Date);
+    public record Report (int TotalQuantity, int TotalRecords);
+}
---
+++ HabitTracker.JoeW980/Program.cs
@@ -0,0 +1,318 @@
+﻿using ConsoleTables;
+using HabitLogger;
+using static HabitLogger.Database;
+bool endAPp = false;
+Database db = new Database();
+
+while (!endAPp)
+{    
+    int menuSelection;
+    int recordSelection;
+    int userQuantity;
+    bool validHabit = false;
+    Habit userHabit;
+    List<Entry> records;
+
+    Console.WriteLine("""
+        Habit Tracker
+        -------------
+        MAIN MENU
+
+        0 - Close Application
+        1 - View All Records
+        2 - Insert Record
+        3 - Delete Record
+        4 - Update Record
+        5 - Add New Habit
+        6 - Reports
+        -------------
+        """);
+    if (!IsHabits())
+    {
+        Console.WriteLine("There are no Habits logged, Please select Insert to add one.");
+    }
+    Console.Write("Select Option: ");
+    menuSelection = ValidateMenu();
+    Console.Clear();
+
+    switch (menuSelection)
+    {
+        case 0:
+            endAPp = true;
+            Console.WriteLine("Goodbye");
+            break;
+
+        case 1:            
+            Console.WriteLine("View Records \n");
+            if (!IsHabits())
+            {
+                Console.WriteLine("No Habits to show, Return to Main Menu to add some.");
+                break;
+            }
+            DisplayRecords();
+            break;
+
+        case 2:
+            Console.WriteLine("Insert Record");
+            Console.WriteLine("-------------");            
+
+            if (!IsHabits())
+            {
+                Console.WriteLine("No Habits to show, Return to Main Menu to add some.");
+                break;
+            }
+
+            validHabit = HabitMenu(out userHabit);
+            if (!validHabit)
+                break;
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(userHabit.HabitName);
+            Console.ResetColor();
+            Console.WriteLine("------");            
+            Console.Write($"Write Quantity in {userHabit.Unit}: ");
+            userQuantity = ValidateMenu();
+            if (userQuantity < 1)
+            {
+                Console.WriteLine("Invalid number - Returning to Main Menu");
+                break;
+            }
+            db.insert(userHabit, userQuantity);
+            Console.WriteLine($"Added {userQuantity} {userHabit.Unit} to {userHabit.HabitName}");
+            break;
+
+        case 3:
+            Console.WriteLine("Delete Record");
+            Console.WriteLine("-------------");
+            if (!IsHabits())
+            {
+                Console.WriteLine("No Habits to show, Return to Main Menu to add some.");
+                break;
+            }
+
+            Console.WriteLine("""
+                1 - Delete Habit & Records
+                2 - Delete Record
+                """);
+            Console.Write("Select Option: ");
+            menuSelection = ValidateMenu();
+            Console.Clear();
+            switch (menuSelection)
+            {
+                case 1:
+                    validHabit = HabitMenu(out userHabit);
+                    if (!validHabit)
+                        break;
+
+                    db.DeleteHabit(userHabit);
+                    break;
+
+                case 2:
+                    validHabit = HabitMenu(out userHabit);
+                    if (!validHabit)
+                        break;
+
+                    Console.Clear();
+                    Console.WriteLine("Select Record to Delete");
+                    Console.WriteLine("-----------------------");
+                    records = db.GetHabitRecords(userHabit);
+                    foreach (var record in records.Select((value, i) => new { i, value }))
+                    {
+                        Console.WriteLine($"{record.i + 1}: {record.value.Quantity} {userHabit.Unit} - {record.value.Date:ddd (dd/MM) HH:mm}");
+
+                    }
+                    Console.WriteLine("0 - Exit to Main Menu");
+                    Console.Write("Select Option: ");
+                    recordSelection = ValidateMenu();
+
+                    if (recordSelection == 0) break;
+                    while (recordSelection > records.Count)
+                    {
+                        Console.Write("Invalid Option, Try again: ");
+                        recordSelection = ValidateMenu();
+                    }
+                    db.DeleteRecord(userHabit, records[recordSelection - 1]);
+                    break;
+            }
+            break;
+
+        case 4:
+            Console.WriteLine("Update Record");
+            Console.WriteLine("-------------");
+            if (!IsHabits())
+            {
+                Console.WriteLine("No Habits to show, Return to Main Menu to add some.");
+                break;
+            }
+            validHabit = HabitMenu(out userHabit);
+            if (!validHabit)
+                break;
+
+            Console.Clear();
+            records = db.GetHabitRecords(userHabit);
+            foreach (var record in records.Select((value, i) => new { i, value }))
+            {
+                Console.WriteLine($"{record.i + 1}: {record.value.Quantity} {userHabit.Unit} - {record.value.Date:ddd (dd/MM) HH:mm}");                
+            }
+            Console.WriteLine("0 - Exit to Main Menu");
+            Console.Write("Select Option: ");
+            recordSelection = ValidateMenu();
+
+            if (recordSelection == 0) break;
+            while (recordSelection > records.Count)
+            {
+                Console.Write("Invalid Option, Try again: ");
+                recordSelection = ValidateMenu();
+            }
+            recordSelection--;
+
+            Console.Clear();
+            Console.WriteLine($"Updating {records[recordSelection].Quantity} {userHabit.Unit} of {userHabit.HabitName}");
+            Console.WriteLine("---------------------");
+            Console.Write("Enter new Quantity: ");
+            userQuantity = ValidateMenu();
+            db.Update(userHabit, records[recordSelection], userQuantity);
+            break;
+
+        case 5:
+            Console.WriteLine("Add Habit");
+            Console.WriteLine("---------");
+            Console.Write("Habit Name: ");
+            string habitName = ValidateInput();
+
+            if (IsHabits())
+            {
+                var existingHabits = new List<string>();
+                foreach (var habit in db.GetHabits())
+                {
+                    existingHabits.Add(habit.HabitName);
+                }
+
+                if (existingHabits.Contains(habitName))
+                {
+                    Console.Write("Habit already exists, Please delete it first.");
+                    break;
+                }
+            }
+            Console.Write("Unit of measurement (e.g. 'Glasses' for Water): ");
+            string unit = ValidateInput();
+
+            db.AddNewHabit(habitName, unit);
+            break;
+
+        case 6:
+            Console.WriteLine("Reporting");
+            Console.WriteLine("---------");
+            if (!IsHabits())
+            {
+                Console.WriteLine("No Habits to show, Return to Main Menu to add some.");
+                break;
+            }
+            validHabit = HabitMenu(out userHabit);
+            if (!validHabit)
+                break;
+            Console.Clear();
+            Console.WriteLine("""
+                1 - Yearly
+                2 - Monthly
+                3 - Weekly
+                """);
+            var timeFrame = ValidateMenu() switch
+            {
+                1 => "Yearly",
+                2 => "Monthly",
+                3 => "Weekly",
+                _ => "Weekly"
+            };
+
+            Console.Clear();
+            Report report = db.GetReport(userHabit, timeFrame);
+
+            Console.WriteLine($"""
+                {userHabit.HabitName} {timeFrame} Report
+                -----------------
+                Total {userHabit.Unit}: {report.TotalQuantity}
+                Total Entries: {report.TotalRecords}
+                """);
+
+            break;
+    }
+    Console.WriteLine("Press any key to continue");
+    Console.ReadKey(true);
+    Console.Clear();
+}
+
+bool IsHabits() => db.GetHabits().Any();
+
+bool HabitMenu(out Habit selectedHabit)
+{
+    var habits = db.GetHabits();
+    foreach (var habit in habits.Select((value, i) => new { i, value }))
+    {
+
+      Console.WriteLine($"{habit.i + 1} - {habit.value.HabitName}");
+    }
+    Console.WriteLine("0 - Exit to Main Menu");
+    Console.Write("Select Option: ");
+    var menuSelection = ValidateMenu();
+    while (menuSelection > habits.Count)
+    {
+        Console.Write("Invalid Option, Try again: ");
+        menuSelection = ValidateMenu();
+    }
+
+    if (menuSelection == 0)
+    {
+        selectedHabit = new Habit(0, "", "", []);
+        return false;
+    }
+    selectedHabit = habits[menuSelection - 1];
+    return true; 
+}
+
+void DisplayRecords()
+{
+    var habits = db.ViewAllRecords();        
+    foreach (var record in habits)
+    {
+        var table = new ConsoleTable("Quantity", "Unit");
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(record.HabitName);
+        Console.ResetColor();
+
+        foreach (var row in record.Entries)
+        {
+            table.AddRow($"{row.Quantity} {record.Unit}", row.Date.Date.ToString("ddd (dd/MM/yy)"));
+        }        
+        table.Write(Format.MarkDown);
+        Console.WriteLine();
+    }
+}
+
+string ValidateInput()
+{
+    string? input = Console.ReadLine();
+    while (String.IsNullOrEmpty(input)) 
+    {
+        Console.Write("Invalid entry, please try again: ");
+        input = Console.ReadLine();
+    }
+
+    return input;
+}
+
+int ValidateMenu()
+{    
+    string? input = Console.ReadLine();
+    int validNumber;
+    while (!int.TryParse(input, out validNumber))
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Value must be a number: ");
+        Console.ResetColor();
+        input = Console.ReadLine();
+    }
+    return validNumber;
+}
