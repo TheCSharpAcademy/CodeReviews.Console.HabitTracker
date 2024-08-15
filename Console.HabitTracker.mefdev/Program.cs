818169dcd383d035fc1fb89da644b425de807b8b
Mefdev <mefdev.pro@gmail.com>
2024-08-16 00:32:15 +0100

    Create: architecture of the project

---
+++ Console.HabitTracker.mefdev/Program.cs
@@ -0,0 +1,28 @@
+﻿using HabitLogger.Models;
+class Application
+{
+    private static void Main()
+    {
+        const string DBNAME = "habit.db";
+        string s_path = GetFilePath(DBNAME);
+        try
+        {
+
+            DBStorage dB = new DBStorage(s_path, DBNAME);
+            Console.ReadLine();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    private static string GetCurrentPath()
+    {
+        return Environment.CurrentDirectory.Replace("bin/Debug/net7.0", "");
+    }
+
+    private static string GetFilePath(string tableName)
+    {
+        return Path.Combine(GetCurrentPath(), tableName);
+    }
+}
