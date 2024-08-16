2243cd437a7a147c5227859d4d0e3dd1cf25c2ac
MansoorAZafar <mansoorahmadzafarwork@gmail.com>
2024-08-16 12:07:38 -0400

    fixed codacy

--- HabitLoggerLibrary.MansoorAZafar/Models/Habits.cs
+++ HabitLoggerLibrary.MansoorAZafar/Models/Habits.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HabitLoggerLibrary.MansoorAZafar.Models
+﻿namespace HabitLoggerLibrary.MansoorAZafar.Models
 {
     public enum HabitSelections
     {
