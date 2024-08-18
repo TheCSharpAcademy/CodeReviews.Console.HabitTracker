c4288d22892ba1debae6798fb5a03c8d8421c7a2
HoRalph <ralph.ho94@gmail.com>
2024-08-17 23:45:18 -0400

    set up sqlite

---
+++ HoRalph.Console.HabbitTracker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace habit_tracker
+{
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = @"Data Source = habit-Tracker.db";
+
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            var tableCmd = connection.CreateCommand();
+
+            tableCmd.CommandText = "";
+
+            tableCmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+
+    }
+
+
+
+
+}
+
+
+
+
+class Next
+{
+    
+}
+
+
+}
