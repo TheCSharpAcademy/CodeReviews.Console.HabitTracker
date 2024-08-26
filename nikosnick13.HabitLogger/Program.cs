59b02d180309d07f7bbf2fab15d2a3c5ab3d3819
NikosNick <nikosnick1995@gmail.com>
2024-08-26 13:54:08 +0300

    finsh the first section of this project

--- nikosnick13.HabitLogger/Program.cs
+++ nikosnick13.HabitLogger/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.Sqlite;
 using System.Collections.Concurrent;
+using System.Data;
 using System.Globalization;
 using System.Reflection;
 using static System.Console;
@@ -8,7 +9,10 @@ namespace nikosnick13.HabitLogger
 {
     internal class Program
     {
+
+
         static string connectionString = @"Data Source=habit-Traker.db";
+
         static void Main(string[] args)
         {
 
@@ -46,9 +50,9 @@ namespace nikosnick13.HabitLogger
             Clear();
             ForegroundColor = ConsoleColor.Green;
 
-            bool isAppRunning = true;
+            bool isAppRunning = false;
 
-            while (isAppRunning)
+            while (!isAppRunning)
             {
                 //WriteLine("============================================");
                 WriteLine("\n\nMAIN Menu");
@@ -65,9 +69,10 @@ namespace nikosnick13.HabitLogger
                 switch (userInput)
                 {
                     case "0":
-                        isAppRunning = false;
+                        WriteLine("\nGoodbye!! \n");
+                        isAppRunning = true;
+                        //Environment.Exit(0);
                         break;
-
                     case "1":
                         Insert();
                         break;
@@ -76,61 +81,72 @@ namespace nikosnick13.HabitLogger
                         break;
 
                     case "3":
-                        //TO DO: Call the Delete Method
+                        Delete();
                         break;
                     case "4":
-                        //TO DO: Call the Update Method
+                        Updade();
                         break;
-
+                    default:
+                        WriteLine("Invalid Command.Please type a number from 0 to 4");
+                        break;
                 }
 
-
             }
-
         }
 
         #region Insert
 
         private static void Insert()
         {
+            Clear();
             string date = GetDateInput();
 
-            int quantity = GetQuantityInput("\n\nPlaese Insert a number of glasses or another measures of your choice (no decimals is allowed \n\n) ");
+            int quantity = GetNumberInput("\n\nPlaese Insert a number of glasses or another measures of your choice (no decimals is allowed \n\n) ");
 
             using (var conn = new SqliteConnection(connectionString))
             {
                 conn.Open();
 
                 var insertUserCmd = conn.CreateCommand();
-
                 insertUserCmd.CommandText = $"INSERT INTO dring_water(date,quantity ) VALUES ('{date}',{quantity})";
-
                 insertUserCmd.ExecuteNonQuery();
-
                 conn.Close();
-
-
             }
 
         }
         //Get the input for Date as string
         private static string GetDateInput()
         {
-            WriteLine("Please insert tha date: DD-MM-YYYY. Type 0 to return to main menu.");
+            WriteLine("Please insert tha date: dd-MM-yyyy. Type 0 to return to main menu.");
 
             string dateInput = ReadLine();
 
             if (dateInput == "0") userInput();
 
-
+            //Validation input for date
+            while (!DateTime.TryParseExact(dateInput, "dd-MM-yyyy", new CultureInfo("en-US"),DateTimeStyles.None,out _)) 
+            {
+                ForegroundColor = ConsoleColor.Red; 
+                Console.WriteLine("\n\nInvalid date. (Format: dd-mm-yyyy). Type 0 to return to main manu or try again:\n\n");
+                ForegroundColor = ConsoleColor.Green;
+                dateInput = Console.ReadLine();
+            }
             return dateInput;
         }
-        //Get the input for quanti as string
-        private static int GetQuantityInput(string msq)
+        //Get the input for quanti as  int
+        private static int GetNumberInput(string msq)
         {
             WriteLine(msq);
 
             string quantityInput = ReadLine();
+            
+
+            //Validation input for quantity
+            while (!Int32.TryParse(quantityInput,out _) || Convert.ToInt32(quantityInput) < 0) 
+            {
+                Console.WriteLine("\n\nInvalid number. Try again.\n\n");
+                quantityInput = Console.ReadLine();
+            }
 
             if (quantityInput == "0") userInput();
 
@@ -144,6 +160,7 @@ namespace nikosnick13.HabitLogger
         #region ViewAll
         private static void VeiwAll()
         {
+           
             using (var conn = new SqliteConnection(connectionString))
             {
                 conn.Open();
@@ -152,6 +169,7 @@ namespace nikosnick13.HabitLogger
                 viewAllCmd.CommandText =
                     $"SELECT * FROM dring_water";
                 viewAllCmd.ExecuteNonQuery();
+
                 //The list is generic how store objects of type DrinkingWater.
                 List<DringWater> tableData = new List<DringWater>();
 
@@ -170,7 +188,6 @@ namespace nikosnick13.HabitLogger
                                 Date = DateTime.ParseExact(reader.GetString(1), "dd-MM-yyyy", new CultureInfo("en-US")),
 
                                 Quantity = reader.GetInt32(2)
-
                             });
                         // Console.WriteLine(String.Format("{0}", reader[0]));
                     }
@@ -181,19 +198,88 @@ namespace nikosnick13.HabitLogger
                         WriteLine("---------------------------------------\n");
                     }
                 }
-                else 
+                else
                 {
-                    WriteLine(" No rows founds");
+                    WriteLine("No rows founds");
+                }
+                conn.Close();
+            }
+        }
+
+        #endregion
+
+        #region Delete
+        private static void Delete()
+        {
+            Clear();
+            VeiwAll();
+
+            var grapId = GetNumberInput("\n\nPlease type the Id of the record you want to delete or type 0 to go back to Main Menu\n\n");
+
+            using (var conn = new SqliteConnection(connectionString))
+            {
+                conn.Open();
+
+                var deleteInsert = conn.CreateCommand();
+
+                deleteInsert.CommandText = $"DELETE FROM dring_water  WHERE Id = '{grapId}'";
+
+                int rowtable = deleteInsert.ExecuteNonQuery();
+
+                if (rowtable == 0)
+                {
+                    WriteLine($"\t\n\nThe reconrd with id {grapId} dosent exist \n\n");
+                    Delete();
+                }
+            }
+            WriteLine($"\t\n\nThe record with id {grapId}  was delete \n\n");
+            userInput();
+        }
+
+
+
+
+        #endregion
+
+        #region Update
+        private static void Updade()
+        {
+            
+            VeiwAll();
+            var grapId = GetNumberInput("\n\nPlease type the Id of the record you want to edit or type 0 to go back to Main Menu \n\n");
+            using (var conn = new SqliteConnection(connectionString))
+            {
+                conn.Open();
+
+                var checkCmd = conn.CreateCommand();
+
+                checkCmd.CommandText = $"SELECT EXISTS(SELECT 1 FROM dring_water WHERE  Id = {grapId})";
+
+                int checkQuary = Convert.ToInt32(checkCmd.ExecuteScalar());
+
+                //We check the record if exist in db
+                if (checkQuary == 0)
+                {
+                     
+                    WriteLine($"\n\n Record with id {grapId} doesen't exist");
+                    conn.Close();
+                   // ReadKey();
+                    Updade();
                 }
 
+                var updateDate = GetDateInput();
+                int updareQuantity = GetNumberInput("\n\nPlaese Update the numbers of glasses or another measures of your choice (no decimals is allowed \n\n");            
+                
+                var editCmd = conn.CreateCommand();
+                editCmd.CommandText = $"UPDATE dring_water SET Date = '{updateDate}',Quantity =  {updareQuantity}  WHERE Id = {grapId}";
 
-
-
-
+                editCmd.ExecuteScalar();
                 conn.Close();
-
-
             }
+
+
+
+
         }
 
         #endregion
