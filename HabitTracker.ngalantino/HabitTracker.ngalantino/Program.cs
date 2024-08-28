00e1582d7334d89ec52b094157a7060d48ed8833
Nicolas Galantino <nick.galantino@gmail.com>
2024-08-28 14:45:48 -0400

    Create database.

---
+++ HabitTracker.ngalantino/HabitTracker.ngalantino/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=habit-Tracker.db";
+
+using (var connection = new SqliteConnection(connectionString)) {
+    // Create database table.
+    connection.Open();
+
+    var tableCmd = connection.CreateCommand();
+
+    tableCmd.CommandText = 
+        @"CREATE TABLE IF NOT EXISTS drinking_water (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT,
+            Quantity INTEGER
+            )";
+
+    tableCmd.ExecuteNonQuery();
+
+    connection.Close();
+}
