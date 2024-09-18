53ec716afd6f600f7cc255d662a9761e493381c1
JorgeMelo26 <jormiguelsm79@gmail.com>
2024-09-18 15:29:26 +0100

    Basic Console Program

---
+++ HabitTracker.BatataDoc3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HabitTracker.BatataDoc3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
