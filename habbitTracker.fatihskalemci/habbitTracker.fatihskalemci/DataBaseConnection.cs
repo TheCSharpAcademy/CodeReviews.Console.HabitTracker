3154f4f3dd08a6839c66ffcd883d2dfab96de292
Fatih S. Kalemci <fatihkalemci96@gmail.com>
2025-01-05 20:46:36 +0300

    Create files

---
+++ habbitTracker.fatihskalemci/habbitTracker.fatihskalemci/DataBaseConnection.cs
@@ -0,0 +1,5 @@
+﻿namespace habbitTracker.fatihskalemci;
+
+internal class DataBaseConnection
+{
+}
---
+++ habbitTracker.fatihskalemci/habbitTracker.fatihskalemci/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = "Data Source=habbit-Tracker.db";
+
+using (var connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+
+    var command = connection.CreateCommand();
+
+    command.CommandText =
+        @"CREATE TABLE IF NOT EXISTS water_tracker (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT,
+            Quantity INTEGER
+            )";
+}
