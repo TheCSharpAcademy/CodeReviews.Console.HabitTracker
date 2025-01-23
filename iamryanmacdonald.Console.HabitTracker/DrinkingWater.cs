54467ddd282cacaa23bd7ca58d44f347b320472f
Ryan Macdonald <ryan@ryanmacdonald.io>
2025-01-24 10:11:19 +1100

    removed unnecessary file

--- iamryanmacdonald.Console.HabitTracker/DrinkingWater.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace iamryanmacdonald.Console.HabitTracker;
-
-internal class DrinkingWater
-{
-    internal DateOnly Date { get; set; }
-    internal int Id { get; set; }
-    internal int Quantity { get; set; }
-}
