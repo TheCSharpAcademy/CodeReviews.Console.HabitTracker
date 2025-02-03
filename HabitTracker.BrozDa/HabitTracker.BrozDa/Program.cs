dc310184d093c7a32f1ca58dc47837267b875859
Daniel <Daniel@DESKTOP-EICR6RV>
2025-02-03 15:50:43 +0100

    Project created

---
+++ HabitTracker.BrozDa/HabitTracker.BrozDa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HabitTracker.BrozDa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
