b12a7261cec3b84b0afd7cb543ebcf901ba990bf
victor cesario <victor.cesario@hotmail.com>
2025-02-04 08:05:00 -0300

    added sqlite package

--- HabitTracker.vcesario/Program.cs
+++ HabitTracker.vcesario/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    * Table layouts:
+    *   HABITDEFS -> table containing different types of habits that the user can log
+    *   HABITLOGS -> table containing actual habits and their values
+*/
+using Microsoft.Data.Sqlite;
+
+using (var connection = new SqliteConnection("Data Source=habittracker.db"))
+{
+    connection.Open();
+
+    // var command = connection.CreateCommand();
+    // command.CommandText =
+    // @"
+    //     SELECT *
+    //     FROM user
+    //     WHERE id = $id
+    // ";
+    // command.Parameters.AddWithValue("$id", 1);
+
+    // using (var reader = command.ExecuteReader())
+    // {
+    //     while (reader.Read())
+    //     {
+    //         var name = reader.GetString(0);
+
+    //         Console.WriteLine($"Hello, {name}!");
+    //     }
+    // }
+}
