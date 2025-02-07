8d54cfb377dd4c5a0f74f9427775f0db87e0e9e3
Daniel <Daniel@DESKTOP-EICR6RV>
2025-02-07 09:59:36 +0100

    4 basic operations implemented

--- HabitTracker.BrozDa/HabitTracker.BrozDa/HabitTracker.cs
+++ HabitTracker.BrozDa/HabitTracker.BrozDa/HabitTracker.cs
@@ -18,6 +18,8 @@
             
             while (true) 
             {
+                Console.Clear();
+                Console.SetCursorPosition(0, 0);
                 PrintMainMenu();
                 ProcessUserInput();
             }
@@ -51,46 +53,53 @@
            
             return numericInput;
         }
-        private void ProcessUserInput() {
+        private void ProcessUserInput() 
+        {
             
             int input = GetUserInput();
             string table = "WaterIntake";
-            List<string> columns;
-            List<DatabaseRecord> records;
-            DatabaseRecord record;
+            //DatabaseRecord record;
 
+            int recordID;
             switch (input)
             {
-                case 1:
-                    columns = _databaseManager.GetTableColumnNames("WaterIntake");
-                    records = _databaseManager.GetTableRecords("WaterIntake");
-                    _IOmanager.PrintRecords(columns, records);
-                    Console.ReadKey();
+                case 1: //print records
+                    _databaseManager.PrintRecordsFromATable(table);
                     break;
-                case 2:
-                    record = _IOmanager.GetNewRecord("WaterIntake");
-                    _databaseManager.InsertRecord(record, "WaterIntake");
-                    Console.WriteLine("\nPress any key to continue");
-                    Console.ReadKey();
+                case 2: //new record
+                    DatabaseRecord newRecord = _IOmanager.GetNewRecord(table);
+                    _databaseManager.InsertRecord(newRecord, table);
                     break;
-                case 3:
-                    columns = _databaseManager.GetTableColumnNames("WaterIntake");
-                    records = _databaseManager.GetTableRecords("WaterIntake");
-                    _IOmanager.PrintRecords(columns, records);
-                    int recordID= _IOmanager.GetRecordIdForUpdate(records);
+                case 3: //update record
+                    _databaseManager.PrintRecordsFromATable(table);
+                    recordID = GetValidRecordID(table, "update");
                     DatabaseRecord oldRecord = _databaseManager.GetRecord(recordID);
-                    int columnPosition = _IOmanager.GetRecordColumnForUpdate(columns);
-                    DatabaseRecord updatedRecord = _IOmanager.GetNewValuesForRecord(oldRecord, columnPosition);
-                    _databaseManager.UpdateRecord(updatedRecord, "WaterIntake");
+                    DatabaseRecord updatedRecord = _IOmanager.UpdateRecord(oldRecord);
+                    _databaseManager.UpdateRecord(updatedRecord, table);
                     break;
-                case 4: break;
-                case 5: break;
-
+                case 4: //delete record
+                    _databaseManager.PrintRecordsFromATable(table);
+                    recordID = GetValidRecordID(table, "delete");
+                    _databaseManager.DeleteRecord(recordID, "WaterIntake");
+                    break;
+                case 5: //exit
+                    break;
                 default:
                     break;
             }
+            Console.WriteLine("\nPress any key to continue");
             Console.ReadKey(true);
-            
+        }
+       public int GetValidRecordID(string table, string operation)
+        {
+            int recordID = _IOmanager.GetRecordIdFromUser("operation");
+            while (!_databaseManager.IsIdPresentInDatabase(recordID, table))
+            {
+                Console.WriteLine("Entered ID is not present in the database");
+                recordID = _IOmanager.GetRecordIdFromUser("update");
+            }
+            return recordID;
+
         }
     }
     
--- HabitTracker.BrozDa/HabitTracker.BrozDa/InputOutputManager.cs
+++ HabitTracker.BrozDa/HabitTracker.BrozDa/InputOutputManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Common;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 namespace HabitTracker.BrozDa
@@ -9,6 +10,7 @@ namespace HabitTracker.BrozDa
         private readonly string _format = "{0,-15:N}";
         private readonly int _IDcolumnWidth = 5;
         private readonly int _OtherColumnsWidth = 15;
+        private readonly int _horizonalLineLength = 5 + 15 + 15 + 4; //5 for ID, 2x15 for Date and Volume, +4 horizontalLines
         public InputOutputManager() { }
 
         public void PrintRecords(List<string> columns, List<DatabaseRecord> records)
@@ -55,7 +57,7 @@ namespace HabitTracker.BrozDa
         public DatabaseRecord GetNewRecord(string table)
         {
             DatabaseRecord newRecord = new DatabaseRecord();
-
+            Console.WriteLine();
             Console.Write($"Please enter value for Date: ");
             newRecord.Date = Console.ReadLine();
             Console.Write($"Please enter value for Volume: ");
@@ -63,19 +65,17 @@ namespace HabitTracker.BrozDa
 
             return newRecord;
         }
-        public int GetRecordIdForUpdate(List<DatabaseRecord> records)
+        public int GetRecordIdFromUser(string operation)
         {
-
-            Console.Write("Please enter ID of the record you wish to update:");
+            Console.Write($"Please valid ID of the record you wish to {operation}:");
             string? input = Console.ReadLine();
-            // MISSING INPUT VERIFICATION CODE
-            while (!ValidateInput(input, 1, records.Count))
+            int numericInput;
+            while(!int.TryParse(input, out numericInput))
             {
-                Console.WriteLine();
-                Console.Write("Please enter ID of the record you wish to update:");
+                Console.Write($"Please valid ID of the record you wish to {operation}:");
                 input = Console.ReadLine();
             }
-            return Convert.ToInt32(input);
+            return numericInput;
         }
         // returns number representing position of columns;
         public int GetRecordColumnForUpdate(List<string> columns)
@@ -93,6 +93,17 @@ namespace HabitTracker.BrozDa
             }
 
             return Convert.ToInt32(input);
+        }
+        public DatabaseRecord UpdateRecord(DatabaseRecord oldRecord)
+        {
+            DatabaseRecord newRecord = new DatabaseRecord(oldRecord.ID, oldRecord.Date, oldRecord.Volume);
+
+            Console.Write("Enter new value for column Date: ");
+            newRecord.Date = Console.ReadLine();
+            Console.Write("Enter new value for column Glasses: ");
+            newRecord.Volume = Console.ReadLine();
+
+            return newRecord;
         }
         public DatabaseRecord GetNewValuesForRecord(DatabaseRecord oldRecord, int columnNumber)
         {
@@ -137,6 +148,51 @@ namespace HabitTracker.BrozDa
 
             return true;
         }
+        public void PrintTableColumns(List<string> columns, string table)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+            PrintTableHeader(table);
+            PrintHorizonalLine();
+
+            //column names
+            Console.Write('|');
+            Console.Write(string.Format("{0,-5:N}", columns[0]));
+            Console.Write('|');
+            Console.Write(string.Format(_format, columns[1]));
+            Console.Write('|');
+            Console.Write(string.Format(_format, columns[2]));
+            Console.Write('|');
+            Console.WriteLine();
+
+            PrintHorizonalLine();
+        }
+        public void PrintRecord(DatabaseRecord record) {
+
+            Console.Write('|');
+            Console.Write(string.Format("{0,-5}", record.ID));
+            Console.Write('|');
+            Console.Write(string.Format(_format, record.Date));
+            Console.Write('|');
+            Console.Write(string.Format(_format, record.Volume));
+            Console.WriteLine('|');
+
+        }
+        public void PrintHorizonalLine()
+        {
+            Console.WriteLine(new string('-', _horizonalLineLength));
+        }
+        private void PrintTableHeader(string table)
+        {
+            string text = $"Table: {table}";
+            int totalSpaces = (_horizonalLineLength - 2 - text.Length);
+            int leftpadding = totalSpaces / 2;
+            int rightPadding = totalSpaces - leftpadding;
+
+            PrintHorizonalLine();
+            Console.WriteLine("|" + new string(' ', leftpadding) + text + new string(' ', rightPadding) + "|");
+        }
+        
         
     }
 }
