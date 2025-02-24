ffc87789a7c6a247311556f0239f25e3cda7e125
StressedBread <martinpolak02@gmail.com>
2025-02-24 11:28:26 +0100

    Initial commit

---
+++ HabitTracker.StressedBread/HabitTracker.StressedBread/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Sqlite;
+
+string connectionString = @"Data Source=HabitTracker.db";
+
+using (SqliteConnection connection = new SqliteConnection(connectionString))
+{
+    connection.Open();
+
+    SqliteCommand tableCommand = connection.CreateCommand();
+
+    tableCommand.CommandText = @"CREATE TABLE IF NOT EXISTS drinking_water ( 
+                                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                Date TEXT,
+                                Quantity INTEGER
+                                )";
+
+    tableCommand.ExecuteNonQuery();
+    connection.Close();
+}
