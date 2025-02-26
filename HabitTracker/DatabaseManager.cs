7265394293f6d1b84a07207e8ec067f12375b6e6
Benzel <benawhiting@gmail.com>
2025-02-26 14:41:27 -0800

    Create method for adding new habits to table

--- HabitTracker/DatabaseManager.cs
+++ HabitTracker/DatabaseManager.cs
@@ -12,19 +12,22 @@ namespace HabitTracker
             connectionString = $"Data Source={dbPath}";
         }
 
+        // Checks for existence of database/table in local file system and creates it if it isn't present
         public void EnsureDatabaseExists()
         {
             try
             {
                 bool dbExists = File.Exists(dbPath);
 
-                using SqliteConnection connection = new SqliteConnection(connectionString);
+                using SqliteConnection connection = new(connectionString);
                 connection.Open();
 
                 if (!dbExists)
                 {
                     Console.WriteLine("Database not found. Creating a new one...");
 
+                    // Opting for 'completed' column instead of 'number_of_times_done' because
+                    // it makes more sense from a UX standpoint imo
                     var createTable = @"
                     CREATE TABLE IF NOT EXISTS habits (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -33,7 +36,7 @@ namespace HabitTracker
                         created_at TEXT DEFAULT CURRENT_TIMESTAMP
                     );";
 
-                    using (var command = new SqliteCommand(createTable, connection))
+                    using (SqliteCommand command = new(createTable, connection))
                     {
                         command.ExecuteNonQuery();
                     }
@@ -48,6 +51,26 @@ namespace HabitTracker
             catch (Exception ex)
             {
                 Console.WriteLine($"Error establishing database connection: {ex.Message}");
+            }
+        }
+
+        // Adds a habit to the table
+        public void AddHabit(string title)
+        {
+            try
+            {
+                using SqliteConnection connection = new(connectionString);
+                connection.Open();
+
+                using SqliteCommand command = new("INSERT INTO habits (title) VALUES (@title);", connection);
+                command.Parameters.AddWithValue("@title", title);
+                command.ExecuteNonQuery();
+
+                Console.WriteLine("Habit added successfully!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing to database: {ex.Message}");
             }
         }
     }
