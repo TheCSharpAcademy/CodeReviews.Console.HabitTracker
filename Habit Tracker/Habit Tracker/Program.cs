764c04ff4a0b63e32761e3071496a027f78fc101
Andrewki44 <andrewkilbourn1996@gmail.com>
2025-03-02 23:13:56 +1100

    Initial Commit

---
+++ Habit Tracker/Habit Tracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Habit_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
