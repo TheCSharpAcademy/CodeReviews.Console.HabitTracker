5e736741c98af7d0eae3fd79ec0709ee0a3b55c1
Andrewki44 <andrewkilbourn1996@gmail.com>
2025-03-03 22:46:36 +1100

    Setup DB Tables

--- Habit Tracker/Habit Tracker/Program.cs
+++ Habit Tracker/Habit Tracker/Program.cs
@@ -6,9 +6,53 @@ namespace Habit_Tracker
     {
         static void Main(string[] args)
         {
-            Database db = new Database();
+            SQLite.SQLite db = new SQLite.SQLite();
 
+            do
+            {
+                Console.Clear();
+                
+                Console.WriteLine("~~~ MAIN MENU ~~~\n");
+                Console.WriteLine("What would you like to do?");
 
+                Console.WriteLine("|  #1.\tCreate a log");
+                Console.WriteLine("|  #2.\tUpdate a log");
+                Console.WriteLine("|  #3.\tDelete a log");
+                Console.WriteLine("|  #4.\tView logs");
+                Console.WriteLine("|  #5.\tManage Habits");
+                Console.WriteLine("|  #99.\tInitalize random habit data (Will delete current data!)");
+                Console.WriteLine("|  #0.\tExit Application");
+
+                Console.Write("Type the option number desired:");
+                string? option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "1":
+                        continue;
+
+                    case "2":
+                        continue;
+
+                    case "3":
+                        continue;
+
+                    case "4":
+                        continue;
+
+                    case "5":
+                        continue;
+
+                    case "99":
+                        continue;
+
+                    case "0":
+                        return;
+
+                    default:
+                        continue;
+                }
+            } while (true);
         }
     }
 }
---
+++ Habit Tracker/SQLite/SQLite.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace SQLite
+{
+    public class SQLite
+    {
+        SqliteConnection connection = new SqliteConnection("Data Source = calc.db; foreign keys = true");
+
+        public SQLite()
+        {
+            connection.Open();
+
+            //Retrieve DB tables
+            string commandText = @"
+                SELECT name
+                FROM sqlite_master
+                WHERE type = 'table'
+            ";
+
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText = commandText;
+            SqliteDataReader reader = command.ExecuteReader();
+            List<string> tables = new List<string>();
+
+            while (reader.Read())
+            {
+                tables.Add(reader.GetString(0));
+            }
+
+            //Check for Habit, or create
+            if (!tables.Contains("Habit"))
+            {
+                commandText = @"
+                CREATE TABLE Habit (
+                    Habit       TEXT    PRIMARY KEY,
+                    Description TEXT    NOT NULL,
+                    UoM         TEXT    NOT NULL
+                )";
+                runCommand(commandText);
+            }
+
+            //Check for Log, or create
+            if (!tables.Contains("Log"))
+            {
+                commandText = @"
+                CREATE TABLE Log (
+                    ID          INTEGER     PRIMARY KEY,
+                    Habit       TEXT        NOT NULL,
+                    Count       INTEGER     NOT NULL,
+                    Date        INTEGER     NOT NULL,
+                    FOREIGN KEY (Habit)
+                    REFERENCES Habit (Habit)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE
+                )";
+                runCommand(commandText);
+            }
+        }
+
+        public void runCommand(string commandText)
+        {
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText = commandText;
+
+            command.ExecuteNonQuery();
+        }
+
+        public List<string[]> runSelect(string commandText)
+        {
+            SqliteCommand command = connection.CreateCommand();
+            command.CommandText = commandText;
+
+            SqliteDataReader reader = command.ExecuteReader();
+            List<string[]> content = new List<string[]>();
+
+            //Iterate through returned rows, building a string[], and adding to content
+            while (reader.Read())
+            {
+                string[] row = new string[reader.FieldCount];
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row[i] = reader.GetString(i);
+                }
+                content.Add(row);
+            }
+            
+            return content;
+        }
+
+        ~SQLite()
+        {
+            //Make sure to close connection on destructor
+            connection.Close();
+        }
+    }
+}
