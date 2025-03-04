a8e56f4bf4ef26b755d8ea726205b6bda3a4a5f7
Andrewki44 <andrewkilbourn1996@gmail.com>
2025-03-04 22:13:38 +1100

    Habit Creation

--- Habit Tracker/Habit Tracker/Program.cs
+++ Habit Tracker/Habit Tracker/Program.cs
@@ -58,16 +58,99 @@ namespace Habit_Tracker
 
         static void ManageHabits(ref SQLite.SQLite db)
         {
-            Console.Clear();
+            List<string[]> habits = db.GetHabits();
 
-            command
-            List<string[]> habits = db.runSelect()
-            
-            Console.WriteLine("~~~ MANAGE HABITS ~~~\n");
+            do
+            {
+                Console.Clear();
 
-            Console.WriteLine("")
-            Console.Write("Type the option number desired:");
+                Console.WriteLine("~~~ MANAGE HABITS ~~~");
 
+                //Display all current habits
+                string[] habitNames = new string[habits.Count];
+                for (int i = 0; i < habits.Count; i++)
+                {
+                    Console.WriteLine($"|\t{habits[i][0]}");
+                    habitNames[i] = habits[i][0].ToUpper();
+                }
+                Console.WriteLine("|\tType 1 to create a new habit");
+                Console.WriteLine("|\tType 0 to return to the main menu");
+
+                Console.Write("Which habit do you want to manage (type habit name):");
+                string? option = Console.ReadLine();
+
+                //Show habit manage menu, or creation menu, or repeat for invalid input
+                if (option != null && habitNames.Contains(option.ToUpper()))
+                {
+                    Console.WriteLine($"\n~~~ Managing habit: {option} ~~~");
+                    Console.WriteLine("| #1. Update");
+                    Console.ReadLine();
+                    return;
+                }
+                else
+                {
+                    //Create new habit, or exit to menu
+                    switch (option)
+                    {
+                        case "1":   //Create habit
+                            string habitName;
+                            string habitDescription;
+                            string habitUoM;
+
+                            //Gather new habit info, ensure no empty strings
+                            Console.WriteLine("\nNew habit name: ");
+                            habitName = GetInput();
+
+                            Console.WriteLine("New habit description: ");
+                            habitDescription = GetInput();
+
+                            Console.WriteLine("New habit UoM: ");
+                            habitUoM = GetInput();
+
+                            try
+                            {
+                                db.CreateHabit(habitName, habitDescription, habitUoM);
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Failed to create habit...");
+                                Console.WriteLine(e.Message);
+                                Console.WriteLine("Press Enter to return to the menu");
+                                continue;
+                            }
+                            finally
+                            {
+                                habits.Add(new string[3] { habitName, habitDescription, habitUoM });
+                            }
+                            break;
+
+                        case "0":   //Exit to menu
+                            return;
+
+                        default:
+                            break;
+                    }
+                }
+            } while (true);
+        }
+
+        /// <summary>
+        /// Enforces non-null user input
+        /// </summary>
+        /// <returns></returns>
+        static string GetInput()
+        {
+            do
+            {
+                string? input = Console.ReadLine();
+
+                if (input == null || input.Length == 0)
+                {
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    Console.Write("\r" + new string(' ', Console.BufferWidth) + "\r");
+                }
+                else return input;
+            } while (true);
         }
     }
 }
--- Habit Tracker/SQLite/SQLite.cs
+++ Habit Tracker/SQLite/SQLite.cs
@@ -12,23 +12,23 @@ namespace SQLite
             connection.Open();
 
             //Retrieve DB tables
-            List<string> tables = GetHabits();
+            List<string> tables = GetTables();
 
             //Check for Habit table, or create
             if (!tables.Contains("Habit"))
             {
-                runCommand(@"
+                RunCommand(@"
                 CREATE TABLE Habit (
                     Habit       TEXT    PRIMARY KEY,
                     Description TEXT    NOT NULL,
-                    UoM         TEXT    NOT NULL
+                    UoM         TEXT    NOT NULL;
                 )");
             }
 
             //Check for Log table, or create
             if (!tables.Contains("Log"))
             {
-                runCommand(@"
+                RunCommand(@"
                 CREATE TABLE Log (
                     ID          INTEGER     PRIMARY KEY,
                     Habit       TEXT        NOT NULL,
@@ -37,61 +37,114 @@ namespace SQLite
                     FOREIGN KEY (Habit)
                     REFERENCES Habit (Habit)
                         ON DELETE CASCADE
-                        ON UPDATE CASCADE
+                        ON UPDATE CASCADE;
                 )");
             }
         }
 
-        private void runCommand(string commandText)
+        public List<string> GetTables()
+        {
+            string commandText = @"
+                SELECT name
+                FROM sqlite_master
+                WHERE type = 'table';
+            ";
+
+            SqliteDataReader reader = RunSelect(commandText);
+            return ReturnColumn(reader);
+        }
+
+        public List<string[]> GetHabits()
+        {
+            string commandText = @"
+                SELECT Habit, Description, UoM
+                FROM Habit;
+            ";
+
+            SqliteDataReader reader = RunSelect(commandText);
+            return ReturnRow(reader);
+        }
+
+        public void CreateHabit(string habitName, string habitDescription, string habitUoM)
+        {
+            string commandText = @"
+                INSERT INTO Habit (habit, description, uom)
+                VALUES ($habit, $description, $uom);
+            ";
+
+            KeyValuePair<string, string>[] parameters = new KeyValuePair<string, string>[3]
+            {
+                new KeyValuePair<string, string>("$habit", habitName),
+                new KeyValuePair<string, string>("$description", habitDescription),
+                new KeyValuePair<string, string>("$uom", habitUoM)
+            };
+
+            RunCommand(commandText, parameters);
+        }
+
+        private void RunCommand(string commandText, KeyValuePair<string, string>[]? parameters = null)
         {
             SqliteCommand command = connection.CreateCommand();
             command.CommandText = commandText;
 
+            //If parameters are passed, add to command
+            if (parameters != null)
+            {
+                foreach (KeyValuePair<string, string> kvp in parameters)
+                {
+                    command.Parameters.AddWithValue(kvp.Key, kvp.Value);
+                }
+            }
+
             command.ExecuteNonQuery();
         }
 
-        private SqliteDataReader runSelect(string commandText)
+        private SqliteDataReader RunSelect(string commandText)
         {
             SqliteCommand command = connection.CreateCommand();
             command.CommandText = commandText;
 
             SqliteDataReader reader = command.ExecuteReader();
             return reader;
-            {
-                //List<string[]> content = new List<string[]>();
-
-                ////Iterate through returned rows, building a string[], and adding to content
-                //while (reader.Read())
-                //{
-                //    string[] row = new string[reader.FieldCount];
-                //    for (int i = 0; i < reader.FieldCount; i++)
-                //    {
-                //        row[i] = reader.GetString(i);
-                //    }
-                //    content.Add(row);
-                //}
-
-                //return content;
-            }
         }
 
-        public List<string> GetHabits()
+        /// <summary>
+        /// Returns all rows of a RunSelect() that only has a single column
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private List<string> ReturnColumn(SqliteDataReader reader)
         {
-            string commandText = @"
-                SELECT name
-                FROM sqlite_master
-                WHERE type = 'table'
-            ";
-
-            SqliteDataReader reader = runSelect(commandText);
-            List<string> tables = new List<string>();
+            List<string> content = new List<string>();
 
             while (reader.Read())
             {
-                tables.Add(reader.GetString(0));
+                content.Add(reader.GetString(0));
             }
 
-            return tables;
+            return content;
+        }
+
+        /// <summary>
+        /// Returns all rows of a RunSelect()
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private List<string[]> ReturnRow(SqliteDataReader reader)
+        {
+            List<string[]> content = new List<string[]>();
+
+            while (reader.Read())
+            {
+                string[] row = new string[reader.FieldCount];
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    row[i] = reader.GetString(i);
+                }
+                content.Add(row);
+            }
+
+            return content;
         }
 
         ~SQLite()
