86bb42fd815afded013ec86cbd2e62a372d8b14b
tomi <remioluwatomicephas@gmail.com>
2025-03-14 02:09:17 +0100

    feat: Implemented the dbhelper method
    refactor: changed the Processquery method from prompts to Program

--- Tomi.HabitTracker/Program.cs
+++ Tomi.HabitTracker/Program.cs
@@ -17,7 +17,77 @@ class Progrram
                 Console.WriteLine("bye!");
                 break;
             }
-            HabitsPrompts.ProcessSelectedMenu(selectedMenu, db);
+            ProcessSelectedMenu(selectedMenu, db);
+        }
+    }
+
+    public static void ProcessSelectedMenu(string selectedMenu, DBHelper db)
+    {
+        switch (selectedMenu)
+        {
+            case "1":
+                //View All Records
+                try
+                {
+                    var habitGists = db.ViewAllRecords();
+                    Console.WriteLine($"{"Id".PadRight(10)} | {"Habit".PadRight(20)} | {"Quantity".PadRight(10)} | {"Habit Date".PadRight(15)}");
+
+                    Console.WriteLine(new string('-', 10 + 20 + 10 + 15 + 3));
+
+                    foreach (HabitCompactGist gist in habitGists)
+                    {
+                        gist.PrintHabitGist();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Can't read habit gist at the moment: {e}");
+                }
+                break;
+            case "2":
+                //Insert Record
+                try
+                {
+                    var (dateInsertField, habitInsertField, quantityInsertField) = HabitsPrompts.GetHabitDetails();
+                    db.InsertRecord((dateInsertField, habitInsertField, quantityInsertField));
+                    Console.WriteLine("Habit Gist Added Successfully!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Can't insert habit gist at the moment: {e}");
+                }
+
+                break;
+            case "3":
+                //Delete Record
+                try
+                {
+                    int deleteId = HabitsPrompts.PromptForId();
+                    db.DeleteRecord(deleteId);
+                    Console.WriteLine("Record deleted successfully!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Can't delete habit gist at the moment: {e}");
+                }
+                break;
+            case "4":
+                try
+                {
+                    int updateId = HabitsPrompts.PromptForId();
+                    var (dateInsertField, habitInsertField, quantityInsertField) = HabitsPrompts.GetHabitDetails();
+
+                    db.UpdateRecord(updateId, (dateInsertField, habitInsertField, quantityInsertField));
+                    Console.WriteLine("Habit Gist Successfully!");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Can't update habit gist at the moment: {e}");
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid Selected Menu");
+                break;
         }
     }
 }
