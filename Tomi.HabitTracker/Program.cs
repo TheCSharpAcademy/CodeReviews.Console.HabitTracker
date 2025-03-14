a72f80591a1d44dcb971f1704a8af675f62595ff
tomi <remioluwatomicephas@gmail.com>
2025-03-14 01:36:39 +0100

    feat: implemented tracker entry - Program.cs

--- Tomi.HabitTracker/Program.cs
+++ Tomi.HabitTracker/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tomi.HabitTracker.Data;
+using Tomi.HabitTracker.Prompts;
+
+
+class Progrram
+{
+    public static void Main(string[] args)
+    {
+        DBHelper db = new("Data Source=habbit-logger.db;Version=3;");
+        bool endApp = false;
+
+        while (!endApp)
+        {
+            string selectedMenu = HabitsPrompts.PromptForMenu();
+            if (selectedMenu == "0")
+            {
+                Console.WriteLine("bye!");
+                break;
+            }
+            HabitsPrompts.ProcessSelectedMenu(selectedMenu, db);
+        }
+    }
+}
