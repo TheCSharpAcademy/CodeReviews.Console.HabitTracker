b28708a92f9eb1b0808b0374698425c0f46a1d50
Hieu Truong <htruong6219@gmail.com>
2025-03-21 13:37:32 -0700

    feat/style: add pseudocode + 2 methods Added pseudocode for main method and methods for creating table on start and a basic user input

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace CodeReviews_Console_HabitTracker;
+using Microsoft.Data.Sqlite;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = @"Data Source=habit-Tracker.db";
+
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            // Create table
+            CreateTable(connection);
+
+            // Offer menu to user
+            
+            connection.Close();
+        }
+    }
+
+    // Creates table if existing table does not exist
+    static void CreateTable(SqliteConnection connection)
+    {
+        var tableCmd = connection.CreateCommand();
+
+        tableCmd.CommandText = 
+        @"CREATE TABLE IF NOT EXISTS drinking_water (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Date TEXT,
+            Quantity INTEGER
+            )";
+
+        tableCmd.ExecuteNonQuery();
+    }
+
+    static int GetUserInput()
+    {
+        int parsedInt;
+        string input = Console.ReadLine();
+        while (!Int32.TryParse(input, out parsedInt))
+        {
+            Console.Write("Invalid input: ");
+            input = Console.ReadLine();
+        }
+
+        return parsedInt;
+    }
+}
