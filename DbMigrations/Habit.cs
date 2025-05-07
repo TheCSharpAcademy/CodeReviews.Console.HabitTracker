0cc8b0bec20c02a60fe3c27cae4267054c3bf16b
devjunaeid <junaeidthamid@gmail.com>
2025-05-07 16:01:46 +0600

    Added: Habits Table Migration.

---
+++ DbMigrations/Habit.cs
@@ -0,0 +1,53 @@
+using DotNETConsole.HabitTracker.DB;
+using Microsoft.Data.Sqlite;
+
+namespace DotNETConsole.HabitTracker.DbMigrations;
+
+public class Habit
+{
+    public void Up()
+    {
+        try
+        {
+            var db = new DbConnection();
+            db.Connection.Open();
+            var command = db.Connection.CreateCommand();
+            command.CommandText = @"CREATE TABLE IF NOT EXISTS Habits (
+                                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    TITLE STRING NOT NULL
+                                  );";
+            
+            command.ExecuteNonQuery();
+            Console.WriteLine("Habits Table is created");
+            db.Connection.Close();
+
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Failed to create Habits table");
+        }
+    }
+    
+    public void Down()
+    {
+        var db = new DbConnection();
+        
+        try
+        {
+            db.Connection.Open();
+            
+            var command = db.Connection.CreateCommand();
+            command.CommandText = @"
+                DROP TABLE IF EXISTS Habits;";
+            command.ExecuteNonQuery();
+            Console.WriteLine("Habits Table is dropped");
+            db.Connection.Close();
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Failed to drop Habits table");
+        }
+    }
+}
