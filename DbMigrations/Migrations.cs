fc1f83c870bad20510fdc6a47aad76aa0a4a6a6b
devjunaeid <junaeidthamid@gmail.com>
2025-05-07 16:01:24 +0600

    Added: Migration Down method & habits table.

--- DbMigrations/Migrations.cs
+++ DbMigrations/Migrations.cs
@@ -5,6 +5,16 @@ public class Migrations
     public void Migrate()
     {
         var userMigration = new User();
+        var habitMigration = new Habit();
         userMigration.Up();
+        habitMigration.Up();
+    }
+
+    public void MigrateDown()
+    {
+        var userMigration = new User();
+        var habitMigration = new Habit();
+        userMigration.Down();
+        habitMigration.Down();
     }
 }
