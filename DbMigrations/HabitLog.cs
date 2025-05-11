d7cca834a959752fa62d785d0325d1a4ad66d0c1
devjunaeid <junaeidthamid@gmail.com>
2025-05-11 21:38:51 +0600

    Added: Habit Logs table Migration.

---
+++ DbMigrations/HabitLog.cs
@@ -0,0 +1,56 @@
+﻿using DotNETConsole.HabitTracker.DB;
+using Microsoft.Data.Sqlite;
+
+namespace DotNETConsole.HabitTracker.DbMigrations;
+public class HabitLog
+{
+    public void Up()
+    {
+        try
+        {
+            var db = new DbConnection();
+            db.Connection.Open();
+
+            var command = db.Connection.CreateCommand();
+
+            command.CommandText = @"CREATE TABLE IF NOT EXISTS HabitLogs (
+                                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    DATE DATETIME NOT NULL,
+                                    QUENTITY INT NOT NULL,
+                                    HABITID FOREGIN KEY REFERENCES Habits(ID)
+                                  );";
+
+            command.ExecuteNonQuery();
+            db.Connection.Close();
+            Console.WriteLine("HabitLogs table Created!");
+        }
+        catch (SqliteException ex)
+        {
+            Console.WriteLine("Failed to create HabitLogs table.");
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+
+    public void Down()
+    {
+        var db = new DbConnection();
+
+        try
+        {
+            db.Connection.Open();
+
+            var command = db.Connection.CreateCommand();
+            command.CommandText = @"
+                DROP TABLE IF EXISTS HabitLogs;";
+            command.ExecuteNonQuery();
+            Console.WriteLine("HabitLogs Table is dropped");
+            db.Connection.Close();
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Failed to drop HabitLogs table");
+        }
+    }
+}
--- DbMigrations/Migrations.cs
+++ DbMigrations/Migrations.cs
@@ -6,15 +6,19 @@ public class Migrations
     {
         var userMigration = new User();
         var habitMigration = new Habit();
+        var habitLogMigration = new HabitLog();
         userMigration.Up();
         habitMigration.Up();
+        habitLogMigration.Up();
     }
 
     public void MigrateDown()
     {
         var userMigration = new User();
         var habitMigration = new Habit();
+        var habitLogMigration = new HabitLog();
         userMigration.Down();
         habitMigration.Down();
+        habitLogMigration.Down();
     }
 }
