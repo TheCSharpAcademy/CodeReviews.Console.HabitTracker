2193cdac128dfb1d590ca7c93e61914db9102e06
devjunaeid <junaeidthamid@gmail.com>
2025-05-19 09:20:41 +0600

    Added: HabitLogView Data Model.

---
+++ DataModels/HabitLogView.cs
@@ -0,0 +1,8 @@
+namespace DotNETConsole.HabitTracker.DataModels;
+
+public class HabitLogView
+{
+    public DateTime EntryDate { get; set; }
+    public int Quentity { get; set; }
+    public string HabitTitle { get; set; }
+}
