400f8558ffb9b8769e31a2119df646d06d96dc05
devjunaeid <junaeidthamid@gmail.com>
2025-06-02 15:56:52 +0600

    Added ToString method to HabitLogView.

--- DataModels/HabitLogView.cs
+++ DataModels/HabitLogView.cs
@@ -2,7 +2,13 @@ namespace DotNETConsole.HabitTracker.DataModels;
 
 public class HabitLogView
 {
+    public int LogId { get; set; }
     public DateTime EntryDate { get; set; }
-    public int Quentity { get; set; }
+    public int Quantity { get; set; }
     public string HabitTitle { get; set; }
+
+    public override string ToString()
+    {
+        return $"{HabitTitle}({DateOnly.FromDateTime(EntryDate)}) - {Quantity}";
+    }
 }
