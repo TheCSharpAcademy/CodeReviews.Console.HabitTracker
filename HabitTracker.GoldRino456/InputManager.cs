5a3fc45d4beb026aabe5cf4bdc98da62e868f3f7
Ethan E. <eastwoode375@gmail.com>
2025-06-11 22:31:56 -0500

    InputManager File Created

---
+++ HabitTracker.GoldRino456/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HabitTracker.GoldRino456
+{
+    public sealed class InputManager
+    {
+        #region Singleton Logic & Properties
+        private static InputManager? _instance = null;
+        private static readonly object _instanceLock = new object();
+
+        public static InputManager Instance
+        {
+            get
+            {
+                lock (_instanceLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new InputManager();
+                    }
+                    return _instance;
+                }
+
+            }
+        }
+        #endregion
+    }
+}
