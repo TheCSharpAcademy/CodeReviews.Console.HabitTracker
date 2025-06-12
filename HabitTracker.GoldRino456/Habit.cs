4fd9299c3f6cffd5b578ea53780456736784b9c3
Ethan E. <eastwoode375@gmail.com>
2025-06-11 19:59:17 -0500

    Created Habit Object

---
+++ HabitTracker.GoldRino456/Habit.cs
@@ -0,0 +1,42 @@
+﻿
+
+namespace HabitTracker.GoldRino456
+{
+    public class Habit
+    {
+        int _tableId; //Determines the habit type this habit object belongs to.
+        int _id;
+        DateTime _date;
+        string _habitType;
+        float _quantity;
+        string _unitOfMeasurement;
+
+        public int TableID { get { return _tableId; } set { _tableId = value; } }
+        public int ID { get { return _id; } set { _id = value; } }
+        public DateTime Date { get { return _date; } set { _date = value; } }
+        public string HabitType { get { return _habitType; } set { _habitType = value; } }
+        public float Quantity { get { return _quantity; } set { _quantity = value; } }
+        public string UnitOfMeasurement { get { return _unitOfMeasurement; } set { _unitOfMeasurement = value; } }
+
+
+        //New Habit Object Constructor
+        public Habit(DateTime date, string habitType, float quantity, string unitOfMeasurement)
+        {
+            Date = date;
+            HabitType = habitType;
+            Quantity = quantity;
+            UnitOfMeasurement = unitOfMeasurement;
+        }
+
+        //Existing Habit Object Constructor
+        public Habit(int tableId, int id)
+        {
+            //Fetch data from database?
+        }
+
+        public override string ToString()
+        {
+            return ($"{Date.ToShortDateString()}: {Quantity} {UnitOfMeasurement} of {HabitType}.");
+        }
+    }
+}
---
+++ HabitTracker.GoldRino456/HabitTracker.cs
@@ -0,0 +1,11 @@
+﻿namespace HabitTracker.GoldRino456
+{
+    class HabitTracker
+    {
+        static void Main()
+        {
+            Habit testHabit = new(new DateTime(2025, 6, 11), "Sleep", 5.72f, "Hours");
+            Console.WriteLine(testHabit);
+        }
+    }
+}
