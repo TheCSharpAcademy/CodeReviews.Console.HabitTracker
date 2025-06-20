b2949e46ec98fa5bb7261533ff5b577208ebb2d1
Immanuel Matthews <75655202+ilmfeemster@users.noreply.github.com>
2025-06-20 11:13:00 -0700

    Add console menu.

---
+++ HabitTracker.ilmfeemster/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.Sqlite;
+
+class Program
+{
+    static void Main()
+    {
+        bool menuActive = true;
+
+        System.Console.WriteLine("Welcome to the Habit Tracker");
+        System.Console.WriteLine("Select an option:\n");
+
+        while (menuActive)
+        {
+
+            System.Console.WriteLine("0: Exit Program");
+            System.Console.Write("Your selection: ");
+
+            string input = System.Console.ReadLine();
+
+            switch (input)
+            {
+                case "0":
+                    menuActive = false;
+                    System.Console.WriteLine("Goodbye");
+                    break;
+            }
+
+            // Menu header presented after any input
+            System.Console.WriteLine("\nSelect an option: \n");
+        }
+    }
+}
