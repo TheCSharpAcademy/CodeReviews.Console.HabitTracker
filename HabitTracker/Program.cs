a73221c21284d1932fa96c132393da0ee3d563f5
vagabundoo <evillamorm@gmail.com>
2025-07-06 19:11:44 +0200

    initial syntax

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,3 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Microsoft.Data.Sqlite;
 
-Console.WriteLine("Hello, World!");
+using (var connection = new SqliteConnection("Data Source=HabitTracker.db"))
+{
+    connection.Open();
+    
+    var command = connection.CreateCommand();
+    command.CommandText =
+        """
+        DROP TABLE IF EXISTS habit;
+        CREATE TABLE IF NOT EXISTS habit  (
+            id INTEGER PRIMARY KEY,
+            habit TEXT NOT NULL,
+            habitCount INTEGER NOT NULL
+        );
+
+        INSERT INTO habit values (1, 'drinkingWater', 10);
+        """;
+    command.ExecuteNonQuery();
+
+    using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            var habitName = reader.GetString(1);
+            var habitCount = reader.GetInt32(2);
+            Console.WriteLine($"You have done habit {habitName} {habitCount} times!");
+        }
+    }
+}
