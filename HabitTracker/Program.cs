950ead45a67c5f8ddfb85f183449550c7215c446
vagabundoo <evillamorm@gmail.com>
2025-07-06 21:25:49 +0200

    Insert query working, with way to give user info on what data was inserted

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -3,6 +3,9 @@
 string dataSource = "DataSource=habittracker.db";
 Initialize(dataSource);
 
+var queries = new Queries(dataSource);
+queries.InsertNewHabit("john", "drinkingCoffee", 20, DateTime.Now);
+
 return;
 
 static void Initialize(string dataSource)
@@ -28,17 +31,54 @@ static void Initialize(string dataSource)
 }
 
 
-class Queries(SqliteConnection connection)
+class Queries(string connection)
 {
-    public SqliteConnection Connection = connection; 
-    void InsertNewHabit(string user, string habit, int count, DateTime date) {
+    public SqliteConnection Connection = new SqliteConnection(connection); 
+    public void InsertNewHabit(string user, string habit, int count, DateTime date) {
+        Connection.Open();
+        
         using var command = Connection.CreateCommand();
-        command.CommandText = $"""
+        command.CommandText = @"
                                INSERT INTO
                                    habit (user, habit, count, date)
                                values
-                                   ({user}, {habit}, {count}, {date});
-                               """;
+                                   ($user, $habit, $count, $date);
+                               ";
+        command.Parameters.AddWithValue("$user", user);
+        command.Parameters.AddWithValue("$habit", habit);
+        command.Parameters.AddWithValue("$count", count);
+        command.Parameters.AddWithValue("$date", date);
+        
+        command.ExecuteNonQuery();
+        
+        // to get the row number, so get a more specific query
+        string rowCountQuery = "SELECT COUNT(*) FROM habit;";
+        int rowCount = 0;
+        using (SqliteCommand rowCountCommand = new SqliteCommand(rowCountQuery, Connection))
+        {
+            using var reader = rowCountCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                rowCount = reader.GetInt32(0);
+            }
+        }
+
+        Console.WriteLine($"Amount of rows after insert operation: {rowCount}");
+        
+        string readQuery = $"select * from habit where id = {rowCount};";
+        using (SqliteCommand readCommand = new SqliteCommand(readQuery, Connection))
+        {
+            using var reader = readCommand.ExecuteReader();
+            {
+                //reader.Depth
+                while (reader.Read())
+                {
+                    Console.WriteLine($"Added habit {reader.GetString(2)} to user {reader.GetString(1)}, counted {reader.GetInt32(3)} times, on day: {reader.GetDateTime(4)}");
+                }
+            }
+        }
+        
+        Connection.Close();
     }
 
     void RetrieveHabits(string user)
